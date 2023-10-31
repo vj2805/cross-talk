790632040588685b00eda19241fc0b86d931524d
Vj2805 <vijk502@gmail.com>
2023-10-31 19:04:28 +0530

    ClientProviders

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -1,11 +1,3 @@
 "use client"
 
-import { SessionProvider } from "next-auth/react"
-
-export default function ClientProvider({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <SessionProvider>{children}</SessionProvider>
-}
+export { SessionProvider } from "next-auth/react"

3f8ee89456d68675b268a0b2a3aeb63c6484644b
Vj2805 <vijk502@gmail.com>
2023-10-31 20:34:55 +0530

    globals.d.ts: added Props types

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,3 +1,13 @@
-declare type Nullish<T> = T | null
-declare type Uncertain<T> = T | undefined
-declare type Optional<T> = T | null | undefined
+import { boolean } from "zod"
+import type { VariantProps } from "class-variance-authority"
+
+export type Nullish<T> = T | null
+export type Uncertain<T> = T | undefined
+export type Optional<T> = T | null | undefined
+
+export type PropsWithAsChild<T extends object> = T & { asChild?: boolean }
+export type PropsWithWithInset<T extends object> = T & { withInset?: boolean }
+export type PropsWithVariant<
+  T extends object,
+  V extends (...args: any) => any,
+> = T & VariantProps<V>

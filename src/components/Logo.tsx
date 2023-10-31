498db9df4da0a24b81fba0ebde2f942b52f29252
Vj2805 <vijk502@gmail.com>
2023-10-31 15:54:19 +0530

    Created <Logo/>

---
+++ src/components/Logo.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+import Image from "next/image"
+
+import { AspectRatio } from "./ui/AspectRatio"
+
+export function Logo() {
+  return (
+    <Link
+      href="/"
+      prefetch={false}
+      className="w-72 h-14 flex items-center overflow-hidden"
+    >
+      <AspectRatio
+        ratio={16 / 9}
+        className="flex items-center justify-center"
+      >
+        <Image
+          priority
+          src=""
+          alt="Cross Talk Logo"
+          className="dark:filter dark:invert"
+        />
+      </AspectRatio>
+    </Link>
+  )
+}

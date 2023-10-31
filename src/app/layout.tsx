644c9054c33183dfb2f1846609b646ebad7c8ef8
Vj2805 <vijk502@gmail.com>
2023-10-31 15:37:56 +0530

    Created <Header/>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google"
 
+import { Header } from "@/components/Header"
+
 import type { Metadata } from "next"
 
 import "../styles/globals.css"
@@ -18,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }

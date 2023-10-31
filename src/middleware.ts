0b4c3725f09da01fb1c8d1a3d57ef8b42514dc1a
Vj2805 <vijk502@gmail.com>
2023-10-31 23:13:54 +0530

    Moved auth into src

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,4 @@
-import { withAuth } from "next-auth/middleware"
-
-export default withAuth
+export { default } from "next-auth/middleware"
 
 export const config = {
   matcher: ["/chat", "/chat/:id+", "/register"],

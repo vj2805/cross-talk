cf8234266a2ac5348aa80834a7f79378ee93621d
Vj2805 <vijk502@gmail.com>
2023-10-31 16:07:48 +0530

    App: layout: wrapped with ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google"
 
 import { Header } from "@/components/Header"
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 import type { Metadata } from "next"
 
@@ -21,8 +22,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <ThemeProvider
+          enableSystem
+          disableTransitionOnChange
+          attribute="class"
+          defaultTheme="system"
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )

cd4d18190ce33cf32a2dfa06775fe6837223818f
Vj2805 <vijk502@gmail.com>
2023-11-01 22:24:40 +0530

    PrcingCards: extracted out getPricingTiers

--- src/components/PricingCards.tsx
+++ src/components/PricingCards.tsx
@@ -1,40 +1,6 @@
 import { CheckIcon } from "lucide-react"
 import Link from "next/link"
-// import { CheckoutButton } from "./CheckoutButton"
-
-const tiers = [
-  {
-    description: "Get chatting right away with anyone, anywhere!",
-    features: [
-      "20 Message Chat Limit in Chats",
-      "2 Participant limit in Chat",
-      "3 Chat Rooms limit",
-      "Supports 2 languages",
-      "48-hour support response time",
-      1,
-    ],
-    href: "#",
-    id: "starter_ID",
-    name: "Starter",
-    priceMonthly: null,
-  },
-  {
-    description: "Unlock the Full Potential with Pro!",
-    features: [
-      "Unlimited Messages in Chats",
-      "Unlimited Participants in Chats",
-      "Unlimited Chat Rooms",
-      "Supports up to 10 languages",
-      "Multimedia support in chats (coming soon)",
-      "1-hour, dedicated support response time",
-      "Early access to New Features",
-    ],
-    href: "#",
-    id: "si_OnlcsLNQYbMVzV",
-    name: "Pro",
-    priceMonthly: "199",
-  },
-]
+import { getPricingTiers } from "@/configs/getPricingTiers"
 
 interface PricingCardsProps {
   redirect: boolean
@@ -43,7 +9,7 @@ export function PricingCards({ redirect }: PricingCardsProps) {
   return (
     <div>
       <div className="mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2">
-        {tiers.map(tier => (
+        {getPricingTiers().map(tier => (
           <div
             key={tier.id}
             className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10"
---
+++ src/configs/getPricingTiers.ts
@@ -0,0 +1,37 @@
+const PRICING_TIERS = [
+  {
+    description: "Get chatting right away with anyone, anywhere!",
+    features: [
+      "20 Message Chat Limit in Chats",
+      "2 Participant limit in Chat",
+      "3 Chat Rooms limit",
+      "Supports 2 languages",
+      "48-hour support response time",
+      1,
+    ],
+    href: "#",
+    id: "starter_ID",
+    name: "Starter",
+    priceMonthly: null,
+  },
+  {
+    description: "Unlock the Full Potential with Pro!",
+    features: [
+      "Unlimited Messages in Chats",
+      "Unlimited Participants in Chats",
+      "Unlimited Chat Rooms",
+      "Supports up to 10 languages",
+      "Multimedia support in chats (coming soon)",
+      "1-hour, dedicated support response time",
+      "Early access to New Features",
+    ],
+    href: "#",
+    id: "si_OnlcsLNQYbMVzV",
+    name: "Pro",
+    priceMonthly: "199",
+  },
+]
+
+export function getPricingTiers() {
+  return PRICING_TIERS
+}

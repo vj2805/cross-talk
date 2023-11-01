b8fe90f391d4a01b533cc6af918ce705ad4cedb3
Vj2805 <vijk502@gmail.com>
2023-11-01 16:38:51 +0530

    UserAvatar: made name and image optional

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -2,29 +2,33 @@ import Image from "next/image"
 import { cn } from "@/utilities/shadcn"
 import { Avatar } from "./ui/avatar/Avatar"
 import { AvatarFallback } from "./ui/avatar/AvatarFallback"
+import type { Optional } from "@/types/globals"
 
 interface UserAvatarProps {
-  name: string
-  image: string
+  name?: Optional<string>
+  image?: Optional<string>
   className?: string
 }
 
 export function UserAvatar({ image, name, className }: UserAvatarProps) {
   return (
     <Avatar className={cn("bg-white text-black", className)}>
-      <Image
-        src={image}
-        alt={name}
-        width={40}
-        height={40}
-        className="rounded-full"
-      />
+      {name && image && (
+        <Image
+          src={image}
+          alt={name}
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+      )}
       <AvatarFallback
         delayMs={1000}
         className="dark:bg-white dark:text-black text-lg"
       >
         {name
-          .split(" ")
+          ?.split(" ")
+          .slice(0, 2)
           .map(n => n[0])
           .join("")}
       </AvatarFallback>

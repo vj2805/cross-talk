02f82ff6b5878b1b194ae8748a942cbde0cc0187
Vj2805 <vijk502@gmail.com>
2023-11-01 17:02:24 +0530

    CreateChatButton: done ui

--- src/components/CreateChatButton.tsx
+++ src/components/CreateChatButton.tsx
@@ -1,3 +1,23 @@
+"use client"
+
+import { MessageSquarePlusIcon } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { Button } from "./ui/button/Button"
+
 export function CreateChatButton() {
-  return <div>CreateChatButton</div>
+  const router = useRouter()
+
+  async function createChat() {
+    router.push(`/chat/${"abc"}`)
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      className="aspect-square p-0"
+      onClick={createChat}
+    >
+      <MessageSquarePlusIcon />
+    </Button>
+  )
 }

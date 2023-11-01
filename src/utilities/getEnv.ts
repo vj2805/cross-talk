42e791caaf12a111aca37e566f62ba90c7d6a882
Vj2805 <vijk502@gmail.com>
2023-11-01 21:07:59 +0530

    getEnv: added firebase props

--- src/utilities/getEnv.ts
+++ src/utilities/getEnv.ts
@@ -1,6 +1,9 @@
 import * as zod from "zod"
 
 const EnvSchema = zod.object({
+  FIREBASE_CLIENT_EMAIL: zod.string(),
+  FIREBASE_PRIVATE_KEY: zod.string(),
+  FIREBASE_PROJECT_ID: zod.string(),
   GOOGLE_CLIENT_ID: zod.string(),
   GOOGLE_CLIENT_SECRET: zod.string(),
 })

62e59980a8c73ec875db103dafad9728e17330f2
Vj2805 <vijk502@gmail.com>
2023-11-01 16:55:15 +0530

    UserButton: Added SignIn SignOut

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,3 +1,38 @@
-export function UserButton() {
-  return <div>UserButton</div>
+"use client"
+
+import { signOut } from "next-auth/react"
+import { SignInButton } from "./SignInButton"
+import { UserAvatar } from "./UserAvatar"
+import { DropdownMenu } from "./ui/dropdown-menu/DropdownMenu"
+import { DropdownMenuContent } from "./ui/dropdown-menu/DropdownMenuContent"
+import { DropdownMenuItem } from "./ui/dropdown-menu/DropdownMenuItem"
+import { DropdownMenuLabel } from "./ui/dropdown-menu/DropdownMenuLabel"
+import { DropdownMenuSeparator } from "./ui/dropdown-menu/DropdownMenuSeparator"
+import { DropdownMenuTrigger } from "./ui/dropdown-menu/DropdownMenuTrigger"
+import type { Session } from "next-auth"
+import type { Nullish } from "@/types/globals"
+
+interface UserButtonProps {
+  session: Nullish<Session>
+}
+
+export function UserButton({ session }: UserButtonProps) {
+  if (!session) {
+    return <SignInButton />
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar
+          name={session.user?.name}
+          image={session.user?.image}
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>Signout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }

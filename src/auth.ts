5d174a407dd0378e98876b83a3327b81e3714a2e
Vj2805 <vijk502@gmail.com>
2023-11-01 21:54:43 +0530

    auth: added firestoreAdapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,27 @@
 import GoogleProvider from "next-auth/providers/google"
+import { FirestoreAdapter } from "@auth/firebase-adapter"
 import { getEnv } from "@/utilities/getEnv"
+import { adminAuth, adminDb } from "./firebase-admin"
 import type { NextAuthOptions } from "next-auth"
 
 export const authOptions: NextAuthOptions = {
+  adapter: FirestoreAdapter(adminDb),
+  callbacks: {
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.sub = user.id
+      }
+      return token
+    },
+    session: async ({ session, token }) => {
+      if (session.user && token.sub) {
+        session.user.id = token.sub
+        session.firebaseToken = await adminAuth.createCustomToken(token.sub)
+      }
+
+      return session
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: getEnv("GOOGLE_CLIENT_ID"),

320685fef8e756069283fcf787ed8e566be5d0cd
Vj2805 <vijk502@gmail.com>
2023-11-01 19:21:14 +0530

    Extracted out Backdrop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,75 @@
+import { MoveRight } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+import { cn } from "@/utilities/shadcn"
+import demo from "@/assets/images/demo.png"
+import { Backdrop } from "@/components/Backdrop"
+
 export default function Home() {
-  return <main className="bg-red-200">Home</main>
+  return (
+    <main className="relative isolate pt-14 dark:bg-gray-900">
+      <Backdrop position="start" />
+      <div className="py-12 sm:py-20 lg:pb-40">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="">
+            <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
+              Let&apos;s CrossTalk.
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
+              You speak your language, they speak their language.{" "}
+              <span className="text-cyan-600 dark:text-cyan-400">
+                Let AI handle the translation.
+              </span>
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                href="/chat"
+                className={cn(
+                  "rounded-md bg-cyan-400 px-3.5 py-2.5",
+                  "text-sm font-semibold text-slate-600 dark:text-cyan-950 shadow-sm",
+                  "hover:bg-cyan-500",
+                  "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-500"
+                )}
+              >
+                Get Started
+              </Link>
+              <Link
+                href="/pricing"
+                className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-300"
+              >
+                View Pricing{" "}
+                <MoveRight
+                  aria-hidden
+                  className="inline ml-1 text-gray-400"
+                />
+              </Link>
+            </div>
+          </div>
+          <div className="mt-16 flow-root sm:mt-24">
+            <div
+              className={cn(
+                "-m-2 p-2",
+                "rounded-xl bg-gray-900/5",
+                "ring-inset ring-gray-900/10",
+                "lg:-m-4 lg:rounded-2xl lg:p-4"
+              )}
+            >
+              <Image
+                unoptimized
+                src={demo}
+                alt="App Screenshot"
+                width={2432}
+                height={1442}
+                className={cn(
+                  "rounded-md shadow-2xl",
+                  "ring-1 ring-gray-900/10"
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Backdrop position="end" />
+    </main>
+  )
 }
---
+++ src/components/Backdrop.tsx
@@ -0,0 +1,37 @@
+import { cn } from "@/utilities/shadcn"
+
+interface BackdropProps {
+  position: "end" | "start"
+}
+
+export function Backdrop({ position }: BackdropProps) {
+  const isEnd = position === "end"
+  return (
+    <div
+      aria-hidden
+      className={cn(
+        "absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl",
+        isEnd ? "top-[calc(100%-30rem)]" : "top-28",
+        isEnd && "sm:top-[calc(100%-30rem)]"
+      )}
+    >
+      <div
+        className={cn(
+          "relative",
+          isEnd ? "left-[calc(50%+3rem)]" : "left-[calc(50%-11rem)]",
+          "aspect-[1155/678] w-[36.125rem]",
+          "-translate-x-1/2",
+          !isEnd && "rotate-[30deg]",
+          "bg-gradient-to-tr from-cyan-500 to-indigo-500 opacity-30",
+          isEnd ? "sm:left-[calc(50%+36rem)]" : "sm:left-[calc(50%-30rem)]",
+          "sm:w-[72.1875rem]"
+        )}
+        style={{
+          clipPath: `polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, ${
+            !isEnd ? "45.2% 58.3%," : ""
+          }45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)`,
+        }}
+      ></div>
+    </div>
+  )
+}

c2be433393834272e4cf9f498e5b8b41403ffb0e
Vj2805 <vijk502@gmail.com>
2023-11-02 17:11:40 +0530

    auth: extracted out functions

--- src/auth.ts
+++ src/auth.ts
@@ -2,33 +2,31 @@ import GoogleProvider from "next-auth/providers/google"
 import { FirestoreAdapter } from "@auth/firebase-adapter"
 import { getEnv } from "@/utilities/getEnv"
 import { adminAuth, adminDb } from "./firebase-admin"
-import type { NextAuthOptions } from "next-auth"
+import type { CallbacksOptions, NextAuthOptions } from "next-auth"
+
+const jwt: CallbacksOptions["jwt"] = async ({ user, token }) => {
+  if (user) {
+    token.sub = user.id
+  }
+  return token
+}
+
+const session: CallbacksOptions["session"] = async ({ session, token }) => {
+  if (session.user && token.sub) {
+    session.user.id = token.sub
+    session.firebaseToken = await adminAuth.createCustomToken(token.sub)
+  }
+  return session
+}
+
+const google = GoogleProvider({
+  clientId: getEnv("GOOGLE_CLIENT_ID"),
+  clientSecret: getEnv("GOOGLE_CLIENT_SECRET"),
+})
 
 export const authOptions: NextAuthOptions = {
   adapter: FirestoreAdapter(adminDb),
-  callbacks: {
-    jwt: async ({ user, token }) => {
-      if (user) {
-        token.sub = user.id
-      }
-      return token
-    },
-    session: async ({ session, token }) => {
-      if (session.user && token.sub) {
-        session.user.id = token.sub
-        session.firebaseToken = await adminAuth.createCustomToken(token.sub)
-      }
-
-      return session
-    },
-  },
-  providers: [
-    GoogleProvider({
-      clientId: getEnv("GOOGLE_CLIENT_ID"),
-      clientSecret: getEnv("GOOGLE_CLIENT_SECRET"),
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
+  callbacks: { jwt, session },
+  providers: [google],
+  session: { strategy: "jwt" },
 }

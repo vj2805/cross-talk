5d9da9ab2989d26d291686e87b838563e20466f2
Vj2805 <vijk502@gmail.com>
2023-11-02 22:47:36 +0530

    created useCreateCheckoutSession

---
+++ src/hooks/useCreateCheckoutSession.ts
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from "react"
+import { onSnapshot } from "firebase/firestore"
+import { addCheckoutSession } from "../services/addCheckoutSession"
+import type { Unsubscribe } from "firebase/firestore"
+import type { Session } from "next-auth"
+
+export function useCreateCheckout({
+  priceId,
+  session,
+}: {
+  session: Local.Nullish<Session>
+  priceId: string
+}) {
+  const [processing, setProcessing] = useState(false)
+  const unsubscribe = useRef<Unsubscribe>()
+
+  async function createCheckoutSession() {
+    if (!session?.user?.id) {
+      return
+    }
+    let docRef
+    try {
+      setProcessing(true)
+      docRef = await addCheckoutSession({
+        priceId,
+        userId: session.user.id,
+      })
+    } catch {
+      setProcessing(false)
+      return
+    }
+    unsubscribe.current?.()
+    unsubscribe.current = onSnapshot(docRef, snap => {
+      const data = snap.data()
+      const url = data?.url
+      const error = data?.error
+      if (error) {
+        alert(`An error occured: ${error.message}`)
+      }
+      if (url) {
+        window.location.assign(url)
+      }
+      setProcessing(false)
+    })
+    return unsubscribe
+  }
+
+  useEffect(() => () => unsubscribe.current?.(), [])
+
+  return { createCheckoutSession, processing }
+}

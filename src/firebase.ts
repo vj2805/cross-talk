95fc79fb73b0788ef6385cc18cfd6abd4a7f380b
Vj2805 <vijk502@gmail.com>
2023-11-02 17:16:17 +0530

    firebase: type annoted

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,8 +2,9 @@ import { getApp, getApps, initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import { getFunctions } from "firebase/functions"
+import type { FirebaseOptions } from "firebase/app"
 
-const firebaseConfig = {
+const config: FirebaseOptions = {
   apiKey: "AIzaSyBu8Hl6s5k8uBbiXYXX4PoFmjr_mqE7Wj4",
   appId: "1:765126335526:web:29c0aeb0d0ec719ea8dae7",
   authDomain: "cross-talk-e6bff.firebaseapp.com",
@@ -12,7 +13,7 @@ const firebaseConfig = {
   storageBucket: "cross-talk-e6bff.appspot.com",
 }
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+const app = getApps().length ? getApp() : initializeApp(config)
 
 export const clientAuth = getAuth(app)
 export const clientRepo = getFirestore(app)

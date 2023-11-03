8c58197fac0d01df29e3ba3324a541490abea38d
Vj2805 <vijk502@gmail.com>
2023-11-03 15:59:59 +0530

    UserAvatar: converted into arrow function

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -9,29 +9,31 @@ interface UserAvatarProps {
   className?: string
 }
 
-export function UserAvatar({ image, name, className }: UserAvatarProps) {
-  return (
-    <Avatar className={cn("bg-white text-black", className)}>
-      {image && (
-        <Image
-          priority
-          src={image}
-          alt={name || "User Image"}
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
-      )}
-      <AvatarFallback
-        delayMs={1000}
-        className="dark:bg-white dark:text-black text-lg"
-      >
-        {name
-          ?.split(" ")
-          .slice(0, 2)
-          .map(n => n[0])
-          .join("")}
-      </AvatarFallback>
-    </Avatar>
-  )
-}
+export const UserAvatar: React.FC<UserAvatarProps> = ({
+  image,
+  name,
+  className,
+}) => (
+  <Avatar className={cn("bg-white text-black", className)}>
+    {image && (
+      <Image
+        priority
+        src={image}
+        alt={name || "User Image"}
+        width={40}
+        height={40}
+        className="rounded-full"
+      />
+    )}
+    <AvatarFallback
+      delayMs={1000}
+      className="dark:bg-white dark:text-black text-lg"
+    >
+      {name
+        ?.split(" ")
+        .slice(0, 2)
+        .map(n => n[0])
+        .join("")}
+    </AvatarFallback>
+  </Avatar>
+)

589dc08532d933f635933d6ef7bc7d7f8dacf2b4
Vj2805 <vijk502@gmail.com>
2023-11-03 16:35:20 +0530

    Using PropsWithRequiredChildren

--- src/providers/SyncSessionWithAuthProvider.tsx
+++ src/providers/SyncSessionWithAuthProvider.tsx
@@ -4,12 +4,8 @@ import { useEffect } from "react"
 import { useSession } from "next-auth/react"
 import { syncSessionWithAuth } from "@/services/syncSessionWithAuth"
 
-interface SyncSessionWithAuthProviderProps {
-  children: React.ReactNode
-}
-
 export const SyncSessionWithAuthProvider: React.FC<
-  SyncSessionWithAuthProviderProps
+  React.PropsWithRequiredChildren
 > = ({ children }) => {
   const { data: session } = useSession()
   useEffect(() => {

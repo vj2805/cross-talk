28e1694b9354264c8a14ac1df96c020084d67222
Vj2805 <vijk502@gmail.com>
2023-11-03 15:40:40 +0530

    GetStartedTodayButton: formatted styles

--- src/components/pricing/GetStartedTodayButton.tsx
+++ src/components/pricing/GetStartedTodayButton.tsx
@@ -5,13 +5,16 @@ export const GetStartedTodayButton: React.FC = () => (
   <Link
     href="/register"
     className={cn(
-      "block mt-8 px-3.5 py-2",
+      "block",
+      "mt-8",
+      "px-3.5 py-2",
       "bg-cyan-500",
       "text-center text-sm font-semibold leading-6 text-white",
-      "rounded-md shadow-sm",
+      "rounded-md",
+      "shadow-sm",
+      "cursor-pointer",
       "hover:bg-cyan-500",
       "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600",
-      "cursor-pointer",
       "disabled:opacity-80"
     )}
   >

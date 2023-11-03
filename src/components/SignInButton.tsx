edf007425496fa28db42ff3957cef9087ba71bb9
Vj2805 <vijk502@gmail.com>
2023-11-03 16:02:15 +0530

    SignInButton: converted into arrow function

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -3,13 +3,11 @@
 import { signIn } from "next-auth/react"
 import { Button } from "./ui/button/Button"
 
-export function SignInButton() {
-  return (
-    <Button
-      variant="outline"
-      onClick={() => signIn()}
-    >
-      Sign In
-    </Button>
-  )
-}
+export const SignInButton: React.FC = () => (
+  <Button
+    variant="outline"
+    onClick={() => signIn()}
+  >
+    Sign In
+  </Button>
+)

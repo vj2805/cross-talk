45684e3353f55318bcf6456e1a3a8127df56d9ea
Vj2805 <vijk502@gmail.com>
2023-11-03 16:03:01 +0530

    Logo: converted into arrow function

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,24 +3,22 @@ import Image from "next/image"
 import logo from "@/assets/images/logo.png"
 import { AspectRatio } from "./ui/aspect-ratio/AspectRatio"
 
-export function Logo() {
-  return (
-    <Link
-      href="/"
-      prefetch={false}
-      className="p-8 w-72 h-14 flex items-center overflow-hidden"
+export const Logo: React.FC = () => (
+  <Link
+    href="/"
+    prefetch={false}
+    className="p-8 w-72 h-14 flex items-center overflow-hidden"
+  >
+    <AspectRatio
+      ratio={16 / 9}
+      className="flex items-center justify-center"
     >
-      <AspectRatio
-        ratio={16 / 9}
-        className="flex items-center justify-center"
-      >
-        <Image
-          priority
-          src={logo}
-          alt="Cross Talk Logo"
-          className="dark:filter dark:invert"
-        />
-      </AspectRatio>
-    </Link>
-  )
-}
+      <Image
+        priority
+        src={logo}
+        alt="Cross Talk Logo"
+        className="dark:filter dark:invert"
+      />
+    </AspectRatio>
+  </Link>
+)

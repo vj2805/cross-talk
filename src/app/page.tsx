2c43b3da94921e7495ee89627e98735f841acf4e
Vj2805 <vijk502@gmail.com>
2023-11-03 16:57:23 +0530

    Home: reformatted classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,38 @@ import { BlobBackdrop } from "@/components/backdrops/BlobBackdrop"
 
 export default function Home() {
   return (
-    <main className="relative isolate pt-14 dark:bg-gray-900">
+    <main className={cn("isolate", "pt-14", "dark:bg-gray-900", "relative")}>
       <BlobBackdrop position="start" />
       <div className="py-12 sm:py-20 lg:pb-40">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="">
+        <div className={cn("mx-auto", "max-w-7xl", "px-6 lg:px-8")}>
+          <div className={cn("mx-auto", "max-w-2xl", "text-center")}>
             <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
               Let&apos;s CrossTalk.
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
+            <p
+              className={cn(
+                "mt-6",
+                "text-lg leading-8 text-gray-600 dark:text-gray-300"
+              )}
+            >
               You speak your language, they speak their language.{" "}
               <span className="text-cyan-600 dark:text-cyan-400">
                 Let AI handle the translation.
               </span>
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
+            <div
+              className={cn(
+                "mt-10",
+                "flex items-center justify-center gap-x-6"
+              )}
+            >
               <Link
                 href="/chat"
                 className={cn(
-                  "rounded-md bg-cyan-400 px-3.5 py-2.5",
-                  "text-sm font-semibold text-slate-600 dark:text-cyan-950 shadow-sm",
+                  "px-3.5 py-2.5",
+                  "bg-cyan-400",
+                  "text-sm font-semibold text-slate-600 dark:text-cyan-950",
+                  "rounded-md shadow-sm",
                   "hover:bg-cyan-500",
                   "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-500"
                 )}
@@ -40,18 +52,19 @@ export default function Home() {
                 View Pricing{" "}
                 <MoveRight
                   aria-hidden
-                  className="inline ml-1 text-gray-400"
+                  className={cn("inline", "ml-1", "text-gray-400")}
                 />
               </Link>
             </div>
           </div>
-          <div className="mt-16 flow-root sm:mt-24">
+          <div className={cn("mt-16 sm:mt-24", "flow-root")}>
             <div
               className={cn(
-                "-m-2 p-2",
-                "rounded-xl bg-gray-900/5",
-                "ring-inset ring-gray-900/10",
-                "lg:-m-4 lg:rounded-2xl lg:p-4"
+                "-m-2 lg:-m-4",
+                "p-2 lg:p-4",
+                "bg-gray-900/5",
+                "rounded-xl lg:rounded-2xl",
+                "ring-inset ring-gray-900/10"
               )}
             >
               <Image

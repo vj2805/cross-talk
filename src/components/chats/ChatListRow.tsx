a5d025e13526572cda47dc964e73c5b2d72d59da
Vj2805 <vijk502@gmail.com>
2023-11-04 18:46:13 +0530

    ChatListRow: created

---
+++ src/components/chats/ChatListRow.tsx
@@ -0,0 +1,9 @@
+import type { Chat } from "@/types/Chat"
+
+interface ChatListRowProps {
+  initialChats: Chat[]
+}
+
+export const ChatListRow: React.FC<ChatListRowProps> = () => {
+  return <div>ChatListRow</div>
+}

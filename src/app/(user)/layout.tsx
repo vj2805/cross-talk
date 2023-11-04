d1ba7784bf37db9510e01a7c7f93cff8c40d6fd8
Vj2805 <vijk502@gmail.com>
2023-11-04 14:23:32 +0530

    Created user/layout

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,7 @@
+import { cn } from "@/services/shadcn"
+
+const Layout: React.FC<React.PropsWithRequiredChildren> = ({ children }) => (
+  <div className={cn("mx-auto", "w-full max-w-6xl flex-1", "flex flex-col")}>
+    {children}
+  </div>
+)

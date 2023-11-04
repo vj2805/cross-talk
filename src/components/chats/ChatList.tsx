c339da5c6535cfe7702c75448abe71f01fec3219
Vj2805 <vijk502@gmail.com>
2023-11-04 18:46:03 +0530

    ChatList: complete

--- src/components/chats/ChatList.tsx
+++ src/components/chats/ChatList.tsx
@@ -1,3 +1,15 @@
-export const ChatList: React.FC = () => {
-  return <div>ChatList</div>
+import { getParticipatingChats } from "@/services/getParticipatingChats"
+import { getSession } from "@/services/getSession"
+import { ChatListRow } from "./ChatListRow"
+
+export const ChatList: React.FC = async () => {
+  const session = await getSession()
+
+  if (!session?.user) {
+    return null
+  }
+
+  const initialChats = await getParticipatingChats(session.user.id)
+
+  return <ChatListRow initialChats={initialChats} />
 }

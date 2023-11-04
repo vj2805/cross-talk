0787e26b76e081d8a5f6d3f8abeae6c288793e9f
Vj2805 <vijk502@gmail.com>
2023-11-04 10:53:58 +0530

    SubscriptionProvider: complete

--- src/components/providers/SubscriptionProvider.tsx
+++ src/components/providers/SubscriptionProvider.tsx
@@ -4,23 +4,29 @@ import { useEffect } from "react"
 import { useSession } from "next-auth/react"
 import { onSnapshot } from "firebase/firestore"
 import { subscriptionsRef } from "@/services/collections/subscriptionsRef"
-import { createContext } from "@/services/createContext"
+import { setSubscription } from "@/stores/subscription"
 
-const [SubscriptionContextProvider, useSubscription] =
-  createContext("Subscription")
-
-export const SubscriptionProvider: React.FC = () => {
+export const SubscriptionProvider: React.FC<
+  React.PropsWithRequiredChildren
+> = ({ children }) => {
   const { data: session } = useSession()
 
   useEffect(() => {
     if (!session?.user) {
       return
     }
-    return onSnapshot(subscriptionsRef(session.user.id), snapshot => {
-      if (snapshot.empty) {
-      }
-    })
+    return onSnapshot(
+      subscriptionsRef(session.user.id),
+      snapshot => {
+        if (snapshot.empty) {
+          setSubscription(null)
+        } else {
+          setSubscription(snapshot.docs[0].data())
+        }
+      },
+      console.error
+    )
   }, [session])
 
-  return <div>SubscriptionProvider</div>
+  return <>{children}</>
 }

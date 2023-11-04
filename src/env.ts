306aae8b93b3c9227fc5880815580d782f35df0b
Vj2805 <vijk502@gmail.com>
2023-11-04 13:53:35 +0530

    env: added NODE_ENV

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,7 @@ const EnvSchema = zod.object({
   FIREBASE_PROJECT_ID: zod.string(),
   GOOGLE_CLIENT_ID: zod.string(),
   GOOGLE_CLIENT_SECRET: zod.string(),
+  NODE_ENV: zod.union([zod.literal("development"), zod.literal("production")]),
   STRIPE_SECRET_KEY: zod.string(),
 })
 

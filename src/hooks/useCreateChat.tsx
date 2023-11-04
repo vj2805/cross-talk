7c35ee255dc4500631fd9e8fae904008bd67713e
Vj2805 <vijk502@gmail.com>
2023-11-04 18:47:44 +0530

    useCreateChat: implemented

--- src/hooks/useCreateChat.tsx
+++ src/hooks/useCreateChat.tsx
@@ -1,8 +1,9 @@
 "use client"
+
 import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { useSession } from "next-auth/react"
-import { useSubscriptionStore } from "@/stores/subscription"
+import { useRouter } from "next/navigation"
+import { addChat } from "@/services/addChat"
 import { useToast } from "../components/ui/toast/useToast"
 
 export function useCreateChat() {
@@ -10,11 +11,36 @@ export function useCreateChat() {
   const { data: session } = useSession()
   const { showToast } = useToast()
   const router = useRouter()
-  const subscription = useSubscriptionStore(store => store.subscription)
 
-  const createChat = async () => {
-    router.push(`/chat/${"abc"}`)
+  async function createChat() {
+    if (!session?.user) {
+      return
+    }
+    setLoading(true)
+    const { dismissToast, updateToast } = showToast({
+      description: "Hold tight while we create your new chat...",
+      title: "Creating new chat...",
+      variant: "default",
+    })
+    try {
+      const chatId = await addChat(session.user)
+      updateToast({
+        description: "Redirecting to the new chat, Please wait...",
+        title: "Created new chat!",
+        variant: "success",
+      })
+      router.push(`/chat/${chatId}`)
+    } catch (error) {
+      updateToast({
+        description: JSON.stringify(error),
+        title: "Something Went Wrong!",
+        variant: "destructive",
+      })
+    } finally {
+      dismissToast(2000)
+      setLoading(false)
+    }
   }
 
-  return { createChat }
+  return { createChat, loading }
 }

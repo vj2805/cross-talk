4d6823b5d05a67c55126a75f7e5b1e7e00278f5d
Vj2805 <vijk502@gmail.com>
2023-11-04 18:45:37 +0530

    addChat: modified to follow new model

--- src/services/addChat.ts
+++ src/services/addChat.ts
@@ -1,17 +1,12 @@
-import { addDoc, serverTimestamp } from "firebase/firestore"
+import { addDoc } from "firebase/firestore"
 import { chatsRef } from "./collections/chatRef"
-import { participantsRef } from "./collections/participantRef"
 import type { User } from "next-auth"
 
 export async function addChat(user: User) {
-  const chatRef = await addDoc(chatsRef(), { id: "" })
-  await addDoc(participantsRef(chatRef.id), {
-    chatId: chatRef.id,
-    email: user.email!,
-    image: user.image ?? "",
-    isAdmin: true,
-    timestamp: serverTimestamp(),
-    userId: user.id,
+  const chatRef = await addDoc(chatsRef(), {
+    admin: user.id,
+    id: "",
+    participants: [],
   })
   return chatRef.id
 }

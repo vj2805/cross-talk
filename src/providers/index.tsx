5b622e4742f96f8e7401fecbc4b30e0049fdc125
Vj2805 <vijk502@gmail.com>
2023-11-06 16:37:04 +0530

    @providers: exports

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,8 @@
+export { LanguageProvider } from "./LanguageProvider"
 export { SessionProvider } from "./SessionProvider"
 export { SubscriptionProvider } from "./SubscriptionProvider"
 export { SyncedUserProvider } from "./SyncedUserProvider"
 export { ThemeProvider } from "./ThemeProvider"
 
+export { useLanguage } from "./LanguageProvider"
 export { useIsPro, useSubscription } from "./SubscriptionProvider"

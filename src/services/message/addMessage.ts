f9a0aa6499dd54aa21c507320cbf6a705bd0736e
Vj2805 <vijk502@gmail.com>
2023-11-06 19:14:26 +0530

    addMessage: passing user explicitly

--- src/services/message/addMessage.ts
+++ src/services/message/addMessage.ts
@@ -13,6 +13,11 @@ export async function addMessage({ chatId, input, user }: AddMessageParams) {
     id: "",
     input,
     timestamp: serverTimestamp(),
-    user,
+    user: {
+      displayName: user.displayName,
+      email: user.email,
+      photoURL: user.photoURL,
+      uid: user.uid,
+    },
   })
 }

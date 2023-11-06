030db287607ba329556fbf3470d71cc91998c40a
Vj2805 <vijk502@gmail.com>
2023-11-06 17:35:23 +0530

    @hooks: exports useChatInputForm

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 export { useSession } from "next-auth/react"
 export { useTheme } from "next-themes"
 export { usePathname, useRouter } from "next/navigation"
+export { useChatInputForm } from "./useChatInputForm"
 export { useCheckoutSession } from "./useCheckoutSession"
 export { useCreateChat } from "./useCreateChat"
 export { useParticipatingChats } from "./useParticipatingChats"

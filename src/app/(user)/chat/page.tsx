4d039fcd21b8b46d1b1a1542be3367aaa8434f02
Vj2805 <vijk502@gmail.com>
2023-11-06 17:22:49 +0530

    ChatsPage: removed unused type

--- src/app/(user)/chat/page.tsx
+++ src/app/(user)/chat/page.tsx
@@ -1,12 +1,6 @@
 import { ChatList } from "@components"
 
-interface ChatsPageProps {
-  searchParams: {
-    error: string
-  }
-}
-
-export default function ChatsPage({ searchParams: { error } }: ChatsPageProps) {
+export default function ChatsPage() {
   return (
     <div>
       <ChatList />

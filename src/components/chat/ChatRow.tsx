0294ec15ffc9db06d66ac441ca0292bed444735a
Vj2805 <vijk502@gmail.com>
2023-11-06 16:35:50 +0530

    ChatRow: created

---
+++ src/components/chat/ChatRow.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { useLastMessageInChat } from "@hooks/useLastMessageInChat"
+import { useRouter, useUser } from "@hooks"
+import { Skeleton } from "@ui"
+import { cn, prettifyId } from "@utilities"
+import { UserAvatar } from "../user/UserAvatar"
+import type { Chat } from "@types"
+
+interface ChatRowProps {
+  chatId: Chat["id"]
+}
+
+export const ChatRow: React.FC<ChatRowProps> = ({ chatId }) => {
+  const [messages, loading, error] = useLastMessageInChat(chatId)
+  const [user] = useUser()
+  const router = useRouter()
+
+  if (loading) {
+    return (
+      <div className={cn("p-5", "flex items-center space-x-2")}>
+        <Skeleton className={cn("h-12 w-12", "rounded-full")} />
+        <div className={cn("flex-1", "space-y-2")}>
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-1/4" />
+        </div>
+      </div>
+    )
+  }
+  const message = messages?.[0]
+
+  return (
+    <div
+      onClick={() => router.push(`/chat/${chatId}`)}
+      className={cn(
+        "p-5",
+        "hover:bg-gray-100 dark:hover:bg-slate-700",
+        "flex items-center space-x-2",
+        "cursor-pointer"
+      )}
+    >
+      <UserAvatar
+        name={message?.user.name ?? user?.displayName}
+        image={message?.user.image ?? user?.photoURL}
+      />
+      <div className="flex-1">
+        <p className="font-bold">
+          {message
+            ? (message.user.name ?? user?.displayName)?.split(" ")[0]
+            : "New Chat"}
+        </p>
+        <p className="text-gray-400 line-clamp-1">
+          {message?.translated?.["en"] ?? "Get the conversation started..."}
+        </p>
+      </div>
+      <div className="text-xs text-gray-400 text-right">
+        <p className="mb-auto">
+          {message?.timestamp.toDate().toLocaleTimeString() ??
+            "No messages yet"}
+        </p>
+        <p className="">Chat #{prettifyId(chatId)}...</p>
+      </div>
+    </div>
+  )
+}

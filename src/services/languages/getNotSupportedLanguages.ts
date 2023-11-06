d568fdf84414f1a49bf0cba3a4df46a04044ee7e
Vj2805 <vijk502@gmail.com>
2023-11-06 17:10:39 +0530

    getNotSupportedLanguages: isPro is boolean

--- src/services/languages/getNotSupportedLanguages.ts
+++ src/services/languages/getNotSupportedLanguages.ts
@@ -2,6 +2,6 @@ import { getAvailableLanguages } from "./getAvailableLanguages"
 
 const availableLanguages = getAvailableLanguages()
 
-export function getNotSupportedLanguages(isPro: string) {
+export function getNotSupportedLanguages(isPro: boolean) {
   return isPro ? [] : availableLanguages.slice(2)
 }

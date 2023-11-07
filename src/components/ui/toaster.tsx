63ab21f0f6fae01afdb8cbc6c7de83474385c198
Vj2805 <vijk502@gmail.com>
2023-11-07 16:48:47 +0530

    toaster: arrows

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -7,30 +7,26 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToasts } from "@/components/ui/use-toast"
+} from "./toast"
+import { useToasts } from "./use-toast"
 
-export function Toaster() {
+export const Toaster: React.FC = () => {
   const toasts = useToasts()
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast
-            key={id}
-            {...props}
-          >
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
+      {toasts.map(({ id, title, description, action, ...props }) => (
+        <Toast
+          key={id}
+          {...props}
+        >
+          <div className="grid gap-1">
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && <ToastDescription>{description}</ToastDescription>}
+          </div>
+          {action}
+          <ToastClose />
+        </Toast>
+      ))}
       <ToastViewport />
     </ToastProvider>
   )

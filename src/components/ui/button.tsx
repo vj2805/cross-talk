020fd8a8432187081a9eef5cbbb734b31f65367d
Vj2805 <vijk502@gmail.com>
2023-11-07 14:00:16 +0530

    button: renamed variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,10 +1,10 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority"
-import type { VariantProps } from "class-variance-authority"
 import { cn } from "@/utilities/shadcn"
+import type { VariantProps } from "class-variance-authority"
 
-const buttonVariants = cva(
+const variants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     defaultVariants: {
@@ -35,7 +35,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+    VariantProps<typeof variants> {
   asChild?: boolean
 }
 
@@ -44,7 +44,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ className, size, variant }))}
+        className={cn(variants({ className, size, variant }))}
         ref={ref}
         {...props}
       />
@@ -53,4 +53,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 )
 Button.displayName = "Button"
 
-export { Button, buttonVariants }
+export { Button, variants as buttonVariants }

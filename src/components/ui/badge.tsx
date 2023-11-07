149844cdef351193ffda9eac15e81597810b1f36
Vj2805 <vijk502@gmail.com>
2023-11-07 13:48:27 +0530

    badge: simplified BadgeProps

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { cva } from "class-variance-authority"
 import { cn } from "@/utilities/shadcn"
-import type { VariantProps } from "class-variance-authority"
 
 const variants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
@@ -23,9 +22,10 @@ const variants = cva(
   }
 )
 
-export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof variants> {}
+type BadgeProps = React.PropsWithVariant<
+  React.ComponentPropsWithoutRef<"div">,
+  typeof variants
+>
 
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (

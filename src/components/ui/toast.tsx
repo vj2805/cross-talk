74b2267ef15ca271defc0a55b7d3bdb31b139b38
Vj2805 <vijk502@gmail.com>
2023-11-07 16:33:27 +0530

    toast: reorganised

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -7,22 +7,8 @@ import { X } from "lucide-react"
 import { cn } from "@/utilities/shadcn"
 import type { VariantProps } from "class-variance-authority"
 
-export const ToastProvider = PrimitiveToast.Provider
-
-export const ToastViewport = React.forwardRef<
-  React.ElementRef<typeof PrimitiveToast.Viewport>,
-  React.ComponentPropsWithoutRef<typeof PrimitiveToast.Viewport>
->(({ className, ...props }, ref) => (
-  <PrimitiveToast.Viewport
-    ref={ref}
-    className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
-      className
-    )}
-    {...props}
-  />
-))
-ToastViewport.displayName = PrimitiveToast.Viewport.displayName
+export type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>
+export type ToastActionElement = React.ReactElement<typeof ToastAction>
 
 const toastVariants = cva(
   "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full",
@@ -40,6 +26,23 @@ const toastVariants = cva(
     },
   }
 )
+
+export const ToastProvider = PrimitiveToast.Provider
+
+export const ToastViewport = React.forwardRef<
+  React.ElementRef<typeof PrimitiveToast.Viewport>,
+  React.ComponentPropsWithoutRef<typeof PrimitiveToast.Viewport>
+>(({ className, ...props }, ref) => (
+  <PrimitiveToast.Viewport
+    ref={ref}
+    className={cn(
+      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      className
+    )}
+    {...props}
+  />
+))
+ToastViewport.displayName = PrimitiveToast.Viewport.displayName
 
 export const Toast = React.forwardRef<
   React.ElementRef<typeof PrimitiveToast.Root>,
@@ -112,6 +115,3 @@ export const ToastDescription = React.forwardRef<
   />
 ))
 ToastDescription.displayName = PrimitiveToast.Description.displayName
-
-export type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>
-export type ToastActionElement = React.ReactElement<typeof ToastAction>

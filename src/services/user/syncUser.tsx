7ca6cf461fb5525433f225154e9ab620feb53ee7
Vj2805 <vijk502@gmail.com>
2023-11-07 11:33:47 +0530

    syncUser: converted into await syntax

--- src/services/user/syncUser.tsx
+++ src/services/user/syncUser.tsx
@@ -1,18 +1,17 @@
-import { signInWithCustomToken, signOut, updateProfile } from "firebase/auth"
+import { signInWithCustomToken, signOut } from "firebase/auth"
 import { clientAuth } from "@firebase"
+import { setSyncedUser } from "@stores/syncedUser"
 import type { Session } from "@types"
 
 export async function syncUser(session: Nullish<Session>) {
-  if (!session?.firebaseToken) {
-    signOut(clientAuth)
-  } else {
-    signInWithCustomToken(clientAuth, session.firebaseToken)
-      .then(credential =>
-        updateProfile(credential.user, {
-          displayName: session.user?.name,
-          photoURL: session.user?.image,
-        })
-      )
-      .catch(console.error)
+  try {
+    if (session?.firebaseToken) {
+      await signInWithCustomToken(clientAuth, session.firebaseToken)
+    } else {
+      await signOut(clientAuth)
+    }
+    setSyncedUser(session?.user ?? null)
+  } catch (error) {
+    console.log(error)
   }
 }

d14c97dec8d0565b0fbe6f19eab81172290746a8
Vj2805 <vijk502@gmail.com>
2023-11-07 16:12:14 +0530

    builtins: exports PrimitiveLabel

--- src/components/ui/builtins.tsx
+++ src/components/ui/builtins.tsx
@@ -3,5 +3,6 @@ export { default as NextLink } from "next/link"
 
 export * as PrimitiveAvatar from "@radix-ui/react-avatar"
 export * as PrimitiveDropdownMenu from "@radix-ui/react-dropdown-menu"
+export * as PrimitiveLabel from "@radix-ui/react-label"
 export * as PrimitiveSlot from "@radix-ui/react-slot"
 export * as PrimitiveForm from "react-hook-form"

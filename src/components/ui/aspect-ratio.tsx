0af34ee71d4126942519df0d50585f0198802186
Vj2805 <vijk502@gmail.com>
2023-11-07 13:30:04 +0530

    aspect-ratio: removed intermediate variable

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,7 +1,3 @@
 "use client"
 
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
-
-const AspectRatio = AspectRatioPrimitive.Root
-
-export { AspectRatio }
+export { Root as AspectRatio } from "@radix-ui/react-aspect-ratio"

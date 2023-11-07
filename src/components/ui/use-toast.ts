d9a08adcdb9e4512cbe279dcb5f938e8cd245857
Vj2805 <vijk502@gmail.com>
2023-11-07 12:08:45 +0530

    use-toast: removed REMOVE_TOAST_DELAY

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -4,7 +4,6 @@ import { generateId } from "@utilities"
 import type { ToastActionElement, ToastProps } from "@/components/ui/toast"
 
 const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 1000000
 
 type ToasterToast = ToastProps & {
   id: string

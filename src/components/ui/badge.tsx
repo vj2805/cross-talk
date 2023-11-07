71cb7dea0fc87060cc7bdc18dab5e5430a0e954a
Vj2805 <vijk502@gmail.com>
2023-11-07 13:58:08 +0530

    badge: styles

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -3,20 +3,37 @@ import { cva } from "class-variance-authority"
 import { cn } from "@/utilities/shadcn"
 
 const variants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  cn(
+    "px-2.5 py-0.5",
+    "text-xs font-semibold",
+    "border rounded-full",
+    "focus:outline-none",
+    "focus:ring-2 focus:ring-ring focus:ring-offset-2",
+    "inline-flex items-center",
+    "transition-colors"
+  ),
   {
     defaultVariants: {
       variant: "default",
     },
     variants: {
       variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        default: cn(
+          "bg-primary hover:bg-primary/80",
+          "text-primary-foreground",
+          "border-transparent"
+        ),
+        destructive: cn(
+          "bg-destructive hover:bg-destructive/80",
+          "text-destructive-foreground",
+          "border-transparent"
+        ),
         outline: "text-foreground",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        secondary: cn(
+          "bg-secondary hover:bg-secondary/80",
+          "text-secondary-foreground",
+          "border-transparent"
+        ),
       },
     },
   }

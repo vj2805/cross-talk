2725d416c9af6b59b60b716c2484b8f775f031b3
Vj2805 <vijk502@gmail.com>
2023-11-07 14:16:59 +0530

    builtins: exports PrimitiveSlot

--- src/components/ui/builtins.tsx
+++ src/components/ui/builtins.tsx
@@ -3,3 +3,4 @@ export { default as NextLink } from "next/link"
 
 export * as PrimitiveAvatar from "@radix-ui/react-avatar"
 export { Slot as PrimitiveSlot } from "@radix-ui/react-slot"
+export * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"

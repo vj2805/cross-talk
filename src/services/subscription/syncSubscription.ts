748057ce51d6b2ea9adb5c19f3ca6dd24d22d4df
Vj2805 <vijk502@gmail.com>
2023-11-08 23:27:46 +0530

    @services/subscription

--- src/services/subscription/syncSubscription.ts
+++
@@ -1,9 +0,0 @@
-import { onSnapshot } from "firebase/firestore"
-import { setSubscription } from "@stores"
-import { subscriptionsRef } from "./refs"
-
-export function syncSubscription(userId: string) {
-  return onSnapshot(subscriptionsRef(userId), snapshot => {
-    setSubscription(snapshot.empty ? null : snapshot.docs[0].data())
-  })
-}

64a4f284954930ac7bca35cc32b8de903ca188c0
Vj2805 <vijk502@gmail.com>
2023-11-08 23:32:24 +0530

    FirestoreSubscriptionService: adheres to Subscription

--- src/services/subscription/FirestoreSubscriptionService.ts
+++ src/services/subscription/FirestoreSubscriptionService.ts
@@ -9,27 +9,9 @@ const subscriptionConverter: FirestoreDataConverter<Subscription> = {
   fromFirestore(snapshot, options) {
     const data = snapshot.data(options)
     return {
-      cancel_at: data.cancel_at,
-      cancel_at_period_end: data.cancel_at_period_end,
-      cancelled_at: data.cancelled_at,
-      created: data.created,
-      current_period_end: data.current_period_end,
-      current_period_start: data.current_period_start,
-      ended_at: data.ended_at,
       id: snapshot.id,
-      items: data.items,
-      latest_invoice: data.latest_invoice,
-      metadata: data.metadata,
-      payment_method: data.payment_method,
-      price: data.price,
-      prices: data.prices,
-      product: data.product,
-      quantity: data.quantity,
       role: data.role,
       status: data.status,
-      stripeLink: data.stripeLink,
-      trial_end: data.trial_end,
-      trial_start: data.trial_start,
     }
   },
   toFirestore(subscription) {

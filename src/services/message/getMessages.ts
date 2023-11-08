fb4bbdbd321bb2528f363e6426c7bcd880e1b11f
Vj2805 <vijk502@gmail.com>
2023-11-08 22:10:37 +0530

    @services/messages: deleted

--- src/services/message/getMessages.ts
+++
@@ -1,7 +0,0 @@
-import { getDocs } from "firebase/firestore"
-import { sortedMessagesRef } from "./refs"
-
-export async function getMessages(chatId: string) {
-  const snapshot = await getDocs(sortedMessagesRef(chatId))
-  return snapshot.docs.map(doc => doc.data())
-}
--- src/services/message/getMessagesCount.ts
+++
@@ -1,7 +0,0 @@
-import { getCountFromServer } from "firebase/firestore"
-import { limitedMessagesRef } from "./refs"
-
-export async function getMessagesCount(chatId: string) {
-  const snapshot = await getCountFromServer(limitedMessagesRef(chatId))
-  return snapshot.data().count
-}

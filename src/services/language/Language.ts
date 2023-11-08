9654647c2e507fbe1702e6e3ce7b20c61a2d0d03
Vj2805 <vijk502@gmail.com>
2023-11-09 01:38:19 +0530

    Language

---
+++ src/services/language/Language.ts
@@ -0,0 +1,18 @@
+export type Language = keyof typeof LANGUAGE_CODES
+
+export type LanguageCode = (typeof LANGUAGE_CODES)[Language]
+
+export const LANGUAGE_CODES = {
+  English: "en",
+  French: "fr",
+  German: "de",
+  Hindi: "hi",
+  Japanese: "ja",
+  Kannada: "kn",
+  Malayalam: "ml",
+  Spanish: "es",
+  Tamil: "ta",
+  Telugu: "te",
+} as const
+
+export const AVAILABLE_LANGUAGES = Object.keys(LANGUAGE_CODES) as Language[]

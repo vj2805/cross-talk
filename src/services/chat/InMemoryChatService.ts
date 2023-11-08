8f45bc49ed89bc6c6472e4b22b211ac59e55fa33
Vj2805 <vijk502@gmail.com>
2023-11-08 17:54:14 +0530

    InMemoryChatService: corrected useParticipatingChats

--- src/services/chat/InMemoryChatService.ts
+++ src/services/chat/InMemoryChatService.ts
@@ -1,28 +1,49 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useSyncExternalStore } from "react"
 import { generateId } from "@utilities"
 import type { Chat } from "@types"
 import type { ChatService } from "./ChatService"
 
 let chats: Chat[] = []
 
+const getChats = () => chats
+
+const listeners = new Set<() => void>()
+
+const subscribe = (listener: () => void) => {
+  listeners.add(listener)
+  return () => listeners.delete(listener)
+}
+
+const notify = () => listeners.forEach(listener => listener())
+
+function _createChat(adminId: string) {
+  const id = generateId()
+  chats = chats.toSpliced(-1, 0, {
+    adminId,
+    id,
+    participantsIds: [adminId],
+  })
+  return id
+}
+
 const createChat: ChatService["createChat"] = adminId => {
   return new Promise<string>(resolve => {
     setTimeout(() => {
-      const id = generateId()
-      chats = chats.toSpliced(-1, 0, {
-        adminId,
-        id,
-        participantsIds: [adminId],
-      })
+      const id = _createChat(adminId)
+      notify()
       resolve(id)
     }, 1000)
   })
 }
 
+function _getParticipatingChats(userId: string) {
+  return chats.filter(chat => chat.participantsIds.includes(userId))
+}
+
 const getParticipatingChats: ChatService["getParticipatingChats"] = userId => {
   return new Promise<Chat[]>(resolve => {
     setTimeout(() => {
-      resolve(chats.filter(chat => chat.participantsIds.includes(userId)))
+      resolve(_getParticipatingChats(userId))
     }, 1000)
   })
 }
@@ -31,15 +52,18 @@ const useParticipatingChats: ChatService["useParticipatingChats"] = (
   userId,
   initalChats
 ) => {
-  const [observable, setObservable] = useState<
-    Model.Observable<Chat[], Chat[]>
-  >([initalChats, "loading"])
+  const chats = useSyncExternalStore(subscribe, getChats)
+  const [observable, setObservable] = useState<Model.Observable<Chat[]>>([
+    initalChats,
+    true,
+    undefined,
+  ])
 
   useEffect(() => {
     getParticipatingChats(userId)
-      .then(chats => setObservable([chats, "idle"]))
-      .catch(reason => setObservable([Error(reason), "error"]))
-  }, [userId])
+      .then(chats => setObservable([chats, false, undefined]))
+      .catch(reason => setObservable([undefined, false, Error(reason)]))
+  }, [chats, userId])
 
   return observable
 }

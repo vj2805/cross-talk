37c68cf5be6b809abfd0af20d7a0139932b4cbde
Vj2805 <vijk502@gmail.com>
2023-11-08 22:48:04 +0530

    MessageService: getLastMessage - retruns uncertain Message

--- src/services/message/MessageService.ts
+++ src/services/message/MessageService.ts
@@ -1,7 +1,7 @@
 import type { Message } from "@types"
 
 export interface MessageService {
-  getLastMessage: (chatId: string) => Promise<Nullish<Message>>
+  getLastMessage: (chatId: string) => Promise<Uncertain<Message>>
   getMessages: (chatId: string) => Promise<Message[]>
   getMessagesCount: (chatId: string) => Promise<number>
   postMessage: (

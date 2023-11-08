0cbc7eab56ff88c13a566b989c120d4bf0021793
Vj2805 <vijk502@gmail.com>
2023-11-09 00:26:19 +0530

    useMessages: uses central module

--- src/hooks/useMessages.ts
+++ src/hooks/useMessages.ts
@@ -1,9 +1,29 @@
-import { useCollectionData } from "react-firebase-hooks/firestore"
-import { sortedMessagesRef } from "@services/message"
+import { useEffect, useState } from "react"
+import { getMessages } from "@services/message"
+import { useProcess } from "./useProcess"
 import type { Message } from "@types"
 
 export function useMessages(chatId: string, initialMessages: Message[]) {
-  return useCollectionData(sortedMessagesRef(chatId), {
-    initialValue: initialMessages,
-  })
+  const { error, processing, setError, startProcess, stopProcess } =
+    useProcess()
+
+  const [messages, setMessages] =
+    useState<Uncertain<Message[]>>(initialMessages)
+
+  useEffect(() => {
+    async function fetchMessages() {
+      startProcess()
+      try {
+        setMessages(await getMessages(chatId))
+      } catch (error) {
+        setError(error as Error)
+        setMessages(undefined)
+      } finally {
+        stopProcess()
+      }
+    }
+    fetchMessages()
+  }, [chatId, startProcess, setError, stopProcess])
+
+  return [messages, processing, error] as const
 }

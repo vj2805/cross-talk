48a2eb37d0eba4ee7e8f5fe228522ffff2efb88c
Vj2805 <vijk502@gmail.com>
2023-11-09 00:17:20 +0530

    useParticipatingChats: uses from central services

--- src/hooks/useParticipatingChats.ts
+++ src/hooks/useParticipatingChats.ts
@@ -1,6 +1,28 @@
-import { chatService } from "@services/chat"
+import { useEffect, useState } from "react"
+import { getParticipatingChats } from "@services/chat"
+import { useProcess } from "./useProcess"
 import type { Chat } from "@types"
 
 export function useParticipatingChats(userId: string, initialChats: Chat[]) {
-  return chatService.useParticipatingChats(userId, initialChats)
+  const { error, processing, setError, startProcess, stopProcess } =
+    useProcess()
+
+  const [chats, setChats] = useState<Uncertain<Chat[]>>(initialChats)
+
+  useEffect(() => {
+    async function fetchParticipatingChats() {
+      startProcess()
+      try {
+        setChats(await getParticipatingChats(userId))
+      } catch (error) {
+        setError(error as Error)
+        setChats(undefined)
+      } finally {
+        stopProcess()
+      }
+    }
+    fetchParticipatingChats()
+  }, [setError, startProcess, stopProcess, userId])
+
+  return [chats, processing, error] as const
 }

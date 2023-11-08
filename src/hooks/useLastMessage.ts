318c3e57ca70dc6026d0e7b5fc8f65e072eb1d4c
Vj2805 <vijk502@gmail.com>
2023-11-09 00:30:47 +0530

    useLastMessage: uses central service

--- src/hooks/useLastMessage.ts
+++ src/hooks/useLastMessage.ts
@@ -1,6 +1,27 @@
-import { useCollectionData } from "react-firebase-hooks/firestore"
-import { lastMessageRef } from "@services/message"
+import { useEffect, useState } from "react"
+import { getLastMessage } from "@services/message"
+import { useProcess } from "./useProcess"
+import type { Message } from "@types"
 
 export function useLastMessage(chatId: string) {
-  return useCollectionData(lastMessageRef(chatId))
+  const { error, processing, setError, startProcess, stopProcess } =
+    useProcess()
+
+  const [lastMessage, setLastMessage] = useState<Uncertain<Message>>(undefined)
+
+  useEffect(() => {
+    async function fetchLastMessage() {
+      startProcess()
+      try {
+        setLastMessage(await getLastMessage(chatId))
+      } catch (error) {
+        setError(error as Error)
+        setLastMessage(undefined)
+      } finally {
+        stopProcess()
+      }
+    }
+  }, [chatId, startProcess, setError, stopProcess])
+
+  return [lastMessage, processing, error] as const
 }

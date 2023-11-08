1b599cf4a12b836281cc7a3ea3965030878bd83a
Vj2805 <vijk502@gmail.com>
2023-11-09 00:01:05 +0530

    @stores/subscription: stores pricingTiers

--- src/stores/subscription.ts
+++ src/stores/subscription.ts
@@ -1,13 +1,15 @@
 import { create } from "zustand"
-import type { Subscription } from "@types"
+import type { PricingTier, Subscription } from "@types"
 
 type SubscriptionStore = {
   isPro: boolean
+  pricingTiers: Uncertain<PricingTier[]>
   subscription: Optional<Subscription>
 }
 
 const useSubscriptionStore = create<SubscriptionStore>(() => ({
   isPro: false,
+  pricingTiers: undefined,
   subscription: undefined,
 }))
 
@@ -19,9 +21,17 @@ export function useIsPro() {
   return useSubscriptionStore(store => store.isPro)
 }
 
+export function usePricingTiers() {
+  return useSubscriptionStore(store => store.pricingTiers)
+}
+
 export function setSubscription(subscription: Nullish<Subscription>) {
   useSubscriptionStore.setState({
     isPro: subscription?.status === "active",
     subscription,
   })
+}
+
+export function setPricingTiers(pricingTiers: Uncertain<PricingTier[]>) {
+  useSubscriptionStore.setState({ pricingTiers })
 }

77851103e6d09a5586739fa61de851fc8695c599
Vj2805 <vijk502@gmail.com>
2023-11-08 20:26:26 +0530

    useProcess: added error state

--- src/hooks/useProcess.ts
+++ src/hooks/useProcess.ts
@@ -2,14 +2,22 @@ import { useState } from "react"
 
 export function useProcess() {
   const [processing, setProcessing] = useState(false)
+  const [error, setError] = useState<Nullish<Error>>(null)
 
   function startProcess() {
     setProcessing(true)
+    setError(null)
   }
 
   function stopProcess() {
     setProcessing(false)
   }
 
-  return { processing, startProcess, stopProcess }
+  return {
+    error,
+    processing,
+    setError,
+    startProcess,
+    stopProcess,
+  }
 }

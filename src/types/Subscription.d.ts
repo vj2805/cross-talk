a8a778c5e8b4c3217098625d2da020e41d0b8782
Vj2805 <vijk502@gmail.com>
2023-11-08 23:31:45 +0530

    Subscription: simplified

--- src/types/Subscription.d.ts
+++ src/types/Subscription.d.ts
@@ -1,23 +1,23 @@
-import type { DocumentReference, Timestamp } from "firebase/firestore"
-import type Stripe from "stripe"
+// import type { DocumentReference, Timestamp } from "firebase/firestore"
+// import type Stripe from "stripe"
 
 export type Subscription = {
-  cancel_at: Nullish<Timestamp>
-  cancel_at_period_end: boolean
-  cancelled_at: Nullish<Timestamp>
-  created: Timestamp
-  current_period_end: Timestamp
-  current_period_start: Timestamp
-  ended_at: Nullish<Timestamp>
+  // cancel_at: Nullish<Timestamp>
+  // cancel_at_period_end: boolean
+  // cancelled_at: Nullish<Timestamp>
+  // created: Timestamp
+  // current_period_end: Timestamp
+  // current_period_start: Timestamp
+  // ended_at: Nullish<Timestamp>
   id?: string
-  items: Stripe.SubscriptionItem[]
-  latest_invoice?: string
-  metadata: Record<string, string>
-  payment_method?: string
-  price: DocumentReference
-  prices: DocumentReference[]
-  product: DocumentReference
-  quantity: number
+  // items: Stripe.SubscriptionItem[]
+  // latest_invoice?: string
+  // metadata: Record<string, string>
+  // payment_method?: string
+  // price: DocumentReference
+  // prices: DocumentReference[]
+  // product: DocumentReference
+  // quantity: number
   role: Nullish<string>
   status:
     | "active"
@@ -27,7 +27,7 @@ export type Subscription = {
     | "past_due"
     | "trialing"
     | "unpaid"
-  stripeLink: string
-  trial_end: Nullish<Timestamp>
-  trial_start: Nullish<Timestamp>
+  // stripeLink: string
+  // trial_end: Nullish<Timestamp>
+  // trial_start: Nullish<Timestamp>
 }

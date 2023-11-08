b0795baf2785500133fe68e010640c1409c98ca1
Vj2805 <vijk502@gmail.com>
2023-11-09 00:01:45 +0530

    @stores: deleted index

--- src/stores/index.ts
+++
@@ -1,3 +0,0 @@
-export { setLanguage, useLanguage } from "./language"
-export { setSubscription, useIsPro, useSubscription } from "./subscription"
-export { setSyncedUser, useSyncedUser } from "./syncedUser"

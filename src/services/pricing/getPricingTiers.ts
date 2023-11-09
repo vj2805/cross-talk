507018f6b190ed2d5fdfc699441361020d043aad
Vj2805 <vijk502@gmail.com>
2023-11-09 09:51:08 +0530

    getPricingService: depends on pricing

--- src/services/pricing/getPricingTiers.ts
+++ src/services/pricing/getPricingTiers.ts
@@ -1,7 +1,39 @@
-import { pricingTiers } from "../subscription/InMemorySubscriptionService"
-import type { SubscriptionService } from "../subscription/SubscriptionService"
+import type { PricingService } from "./PricingService"
+import type { PricingTier } from "./PricingTier"
 
-export const getPricingTiers: SubscriptionService["getPricingTiers"] =
-  async () => {
-    return pricingTiers
-  }
+const pricingTiers: PricingTier[] = [
+  {
+    description: "Get chatting right away with anyone, anywhere!",
+    features: [
+      "20 Message Chat Limit in Chats",
+      "2 Participant limit in Chat",
+      "3 Chat Rooms limit",
+      "Supports 2 languages",
+      "48-hour support response time",
+    ],
+    href: "#",
+    id: "Starter",
+  },
+  {
+    description: "Unlock the Full Potential with Pro!",
+    features: [
+      "Unlimited Messages in Chats",
+      "Unlimited Participants in Chats",
+      "Unlimited Chat Rooms",
+      "Supports up to 10 languages",
+      "Multimedia support in chats (coming soon)",
+      "1-hour, dedicated support response time",
+      "Early access to New Features",
+    ],
+    href: "#",
+    id: "Pro",
+    priceMonthly: {
+      cost: "199",
+      id: "price_1O83KkSD4okAqSj5ysyvJBJA",
+    },
+  },
+]
+
+export const getPricingTiers: PricingService["getPricingTiers"] = async () => {
+  return pricingTiers
+}

2492eaa01ea2c0541c2036ce848d7e49ab9139e1
Vj2805 <vijk502@gmail.com>
2023-11-09 11:11:30 +0530

    @services/checkout: deleted refs

--- src/services/checkout/refs.ts
+++
@@ -1,6 +0,0 @@
-import { collection } from "firebase/firestore"
-import { clientRepo } from "@backend/firebase"
-
-export function checkoutSessionsRef(userId: string) {
-  return collection(clientRepo, "customers", userId, "checkout_sessions")
-}

7844270b4f889f9055b829fef1135231c3b9e7f7
Vj2805 <vijk502@gmail.com>
2023-11-09 09:51:29 +0530

    InMemorySubscriptionService: decoupled

--- src/services/subscription/InMemorySubscriptionService.ts
+++ src/services/subscription/InMemorySubscriptionService.ts
@@ -1,39 +1,6 @@
-import type { PricingTier } from "../pricing/PricingTier"
+import { getPricingTiers } from "@services/pricing/getPricingTiers"
 import type { Subscription } from "./Subscription"
 import type { SubscriptionService } from "./SubscriptionService"
-
-export const pricingTiers: PricingTier[] = [
-  {
-    description: "Get chatting right away with anyone, anywhere!",
-    features: [
-      "20 Message Chat Limit in Chats",
-      "2 Participant limit in Chat",
-      "3 Chat Rooms limit",
-      "Supports 2 languages",
-      "48-hour support response time",
-    ],
-    href: "#",
-    id: "Starter",
-  },
-  {
-    description: "Unlock the Full Potential with Pro!",
-    features: [
-      "Unlimited Messages in Chats",
-      "Unlimited Participants in Chats",
-      "Unlimited Chat Rooms",
-      "Supports up to 10 languages",
-      "Multimedia support in chats (coming soon)",
-      "1-hour, dedicated support response time",
-      "Early access to New Features",
-    ],
-    href: "#",
-    id: "Pro",
-    priceMonthly: {
-      cost: "199",
-      id: "price_1O83KkSD4okAqSj5ysyvJBJA",
-    },
-  },
-]
 
 const subscriptions: Map<string, Subscription> = new Map()
 
@@ -41,14 +8,6 @@ const listenersMap: Map<
   string,
   Set<(subscription: Nullish<Subscription>) => void>
 > = new Map()
-
-const getPricingTiers: SubscriptionService["getPricingTiers"] = async () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(pricingTiers)
-    }, 1000)
-  })
-}
 
 const syncSubscription: SubscriptionService["syncSubscription"] = (
   userId,

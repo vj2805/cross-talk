c786b3df117212aef69bb8a36d263b309f2ca046
Vj2805 <vijk502@gmail.com>
2023-11-09 12:42:02 +0530

    useSyncUser: sets syncedUser

--- src/hooks/useSyncUser.ts
+++ src/hooks/useSyncUser.ts
@@ -2,10 +2,11 @@
 import { useEffect } from "react"
 import { useSession } from "@hooks"
 import { syncUser } from "@services/user"
+import { setSyncedUser } from "@stores/syncedUser"
 
 export function useSyncUser() {
   const { data: session } = useSession()
   useEffect(() => {
-    syncUser(session)
+    syncUser(session).then(setSyncedUser)
   }, [session])
 }

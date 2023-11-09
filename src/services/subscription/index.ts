9336adcc7828f27e85b2d528f73a8954e2ea81aa
Vj2805 <vijk502@gmail.com>
2023-11-09 10:02:38 +0530

    @services/subscription: de-exported pricings

--- src/services/subscription/index.ts
+++ src/services/subscription/index.ts
@@ -1,9 +1,6 @@
 import createSubscriptionService from "./InMemorySubscriptionService"
 import type { SubscriptionService } from "./SubscriptionService"
 
-export type { PricingTier } from "@services/pricing"
-export { getPricingTiers } from "@services/pricing"
-
 export type { Subscription } from "./Subscription"
 
 export const { syncSubscription }: SubscriptionService =

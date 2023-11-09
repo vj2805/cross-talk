e0ae37da5c2923a13438e9e2939b866f3cabd6bb
Vj2805 <vijk502@gmail.com>
2023-11-09 10:07:44 +0530

    @stores/subscription: delegates to @stores/pricing

--- src/stores/subscription.ts
+++ src/stores/subscription.ts
@@ -1,16 +1,15 @@
 import { create } from "zustand"
-import type { PricingTier } from "@services/pricing"
 import type { Subscription } from "@services/subscription"
+
+export { setPricingTiers, usePricingTiers } from "./pricing"
 
 type SubscriptionStore = {
   isPro: boolean
-  pricingTiers: Uncertain<PricingTier[]>
   subscription: Optional<Subscription>
 }
 
 const useSubscriptionStore = create<SubscriptionStore>(() => ({
   isPro: false,
-  pricingTiers: undefined,
   subscription: undefined,
 }))
 
@@ -22,17 +21,9 @@ export function useIsPro() {
   return useSubscriptionStore(store => store.isPro)
 }
 
-export function usePricingTiers() {
-  return useSubscriptionStore(store => store.pricingTiers)
-}
-
 export function setSubscription(subscription: Nullish<Subscription>) {
   useSubscriptionStore.setState({
     isPro: subscription?.status === "active",
     subscription,
   })
-}
-
-export function setPricingTiers(pricingTiers: Uncertain<PricingTier[]>) {
-  useSubscriptionStore.setState({ pricingTiers })
 }

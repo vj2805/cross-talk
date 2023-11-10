a5ef25eeb1c7c0bd0b8250370f807a17b45ec136
Vj2805 <vijk502@gmail.com>
2023-11-10 20:17:16 +0530

    PricingService

--- src/types/PricingService.ts
+++ src/types/PricingService.ts
@@ -1,6 +1,5 @@
 import type { PriceTier } from "./PriceTier"
 
 export interface PricingService {
-  createCheckout: (userId: string, priceId: string) => Promise<string>
   getPricingTiers: () => Promise<PriceTier[]>
 }

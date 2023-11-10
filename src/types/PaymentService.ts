f59b732e6f28c780e9f8b11470a5d7a6f422a4c7
Vj2805 <vijk502@gmail.com>
2023-11-10 20:29:22 +0530

    PaymentService: added listenCheckout

--- src/types/PaymentService.ts
+++ src/types/PaymentService.ts
@@ -1,3 +1,9 @@
+import type { Checkout } from "./Checkout"
+
 export interface PaymentService {
   createCheckout: (userId: string, priceId: string) => Promise<string>
+  listenCheckout: (
+    checkoutId: string,
+    listener: (checkout: Checkout) => void
+  ) => void
 }

c31584dba305b0ad3f4a20f77b51bfea8b91f17c
Vj2805 <vijk502@gmail.com>
2023-11-10 21:14:34 +0530

    PaymentService: typed using Checkout

--- src/types/PaymentService.ts
+++ src/types/PaymentService.ts
@@ -2,9 +2,9 @@ import type { Unsubscribe } from "./Unsubscribe"
 import type { Checkout } from "./Checkout"
 
 export interface PaymentService {
-  createCheckout: (userId: string, priceId: string) => Promise<string>
+  createCheckout: (userId: string, priceId: string) => Promise<Checkout["id"]>
   subscribeToCheckout: (
-    checkoutId: string,
+    checkoutId: Checkout["id"],
     listener: (checkout: Checkout) => void
   ) => Unsubscribe
 }

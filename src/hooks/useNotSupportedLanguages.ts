010aa4c7db37976db62934bdb3e2a50d7e3c0dda
Vj2805 <vijk502@gmail.com>
2023-11-11 17:25:58 +0530

    useNotSupportedLanguages: uses ternery

--- src/hooks/useNotSupportedLanguages.ts
+++ src/hooks/useNotSupportedLanguages.ts
@@ -2,10 +2,9 @@ import { shallow } from "zustand/shallow"
 import { useLanguageStore } from "@/stores/language"
 
 export function useNotSupportedLanguages(isPro: boolean) {
-  return useLanguageStore(store => {
-    if (!store.availableLanguages) {
-      return undefined
-    }
-    return isPro ? [] : store.availableLanguages.pro
-  }, shallow)
+  return useLanguageStore(
+    ({ availableLanguages }) =>
+      !availableLanguages ? undefined : isPro ? [] : availableLanguages.pro,
+    shallow
+  )
 }

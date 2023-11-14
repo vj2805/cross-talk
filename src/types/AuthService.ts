8b17080b9f81690d32dcfefca0b5a2073d88a964
Vj2805 <vijk502@gmail.com>
2023-11-14 21:38:14 +0530

    Services

--- src/types/AuthService.ts
+++ src/types/AuthService.ts
@@ -1,9 +1,9 @@
-import type { Mutate, Obtain } from "./Service"
+import type { Mutate } from "./Service"
 import type { User } from "./User"
 import type { Adapter } from "next-auth/adapters"
 
 export interface AuthService {
-  createAuthAdapter: Obtain<Adapter>
+  createAuthAdapter: () => Adapter
   createAuthToken: Mutate<
     {
       userId: User["id"]

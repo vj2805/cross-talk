0779dfdff0f0c35f2a00489e2c45d4e866e9a348
Vj2805 <vijk502@gmail.com>
2023-11-14 18:38:29 +0530

    ChatControls: renders AdminControl and badges

---
+++ src/components/chat/ChatControls.tsx
@@ -0,0 +1,29 @@
+import { useChat } from "@/hooks/useChat"
+import { useUser } from "@/hooks/useUser"
+import { Spinner, showErrorToast } from "../ui"
+import { ChatParticipantsBadges } from "./ChatParticipantsBadges"
+import { AdminControls } from "./AdminControls"
+
+interface ChatControlsProps {
+  chatId: string
+}
+
+export const ChatControls: React.FC<ChatControlsProps> = ({ chatId }) => {
+  const user = useUser()
+  const chat = useChat(chatId)
+
+  if (chat.status === "loading") {
+    return <Spinner />
+  }
+
+  if (chat.status === "error") {
+    return void showErrorToast(chat.error)
+  }
+
+  return (
+    <>
+      {user?.id === chat.value.adminId && <AdminControls chat={chat.value} />}
+      <ChatParticipantsBadges chat={chat.value} />
+    </>
+  )
+}

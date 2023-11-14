ae1110a6b9ba3827ac2cee56d995e3a91d94f65b
Vj2805 <vijk502@gmail.com>
2023-11-14 13:55:18 +0530

    inmemory/user: added getUserByEmail

--- src/services/internal/inmemory/user.ts
+++ src/services/internal/inmemory/user.ts
@@ -1,6 +1,21 @@
+import { getInMemoryState } from "./store"
 import type { UserService } from "@/types/UserService"
 
 const inMemoryUserService: UserService = {
+  async getUserByEmail(email) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const user = getInMemoryState("users").find(
+          user => user.email === email
+        )
+        if (!user) {
+          reject(new Error(`User with email ${email} does not exist!`))
+          return
+        }
+        resolve(user)
+      }, 1000)
+    })
+  },
   syncUser(session) {
     return new Promise(resolve => {
       setTimeout(() => {

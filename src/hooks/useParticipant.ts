516ac5dee3f583d77c70ffdc845eba943785d6cc
Vj2805 <vijk502@gmail.com>
2023-11-14 18:38:41 +0530

    useParticipant: created

---
+++ src/hooks/useParticipant.ts
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+import { subscribeToUser } from "@/services/user"
+import { useObservable } from "./useObservable"
+import type { User } from "@/types/User"
+
+export function useParticipant(participantId: string) {
+  const [participant, setParticipant, setError] = useObservable<User>()
+
+  useEffect(
+    () => subscribeToUser(participantId, setParticipant, setError),
+    [participantId, setParticipant, setError]
+  )
+
+  return participant
+}

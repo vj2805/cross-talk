4d147bb2ae5f59caa13e021b1686aa15e6380bb4
Vj2805 <vijk502@gmail.com>
2023-11-14 13:27:37 +0530

    ui/primitives: exports PrimitiveDialog

--- src/components/ui/primitives.tsx
+++ src/components/ui/primitives.tsx
@@ -2,6 +2,7 @@
 
 export * as PrimitiveAspectRatio from "@radix-ui/react-aspect-ratio"
 export * as PrimitiveAvatar from "@radix-ui/react-avatar"
+export * as PrimitiveDialog from "@radix-ui/react-dialog"
 export * as PrimitiveDropdownMenu from "@radix-ui/react-dropdown-menu"
 export * as PrimitiveLabel from "@radix-ui/react-label"
 export * as PrimitiveSelect from "@radix-ui/react-select"

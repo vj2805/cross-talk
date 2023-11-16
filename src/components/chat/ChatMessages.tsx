42800b523039bf3bfb35f1627660e1f32a85ce9f
Vj2805 <vijk502@gmail.com>
2023-11-16 13:09:16 +0530

    ChatMessages: Code split

--- src/components/chat/ChatMessages.tsx
+++ src/components/chat/ChatMessages.tsx
@@ -1,13 +1,10 @@
 "use client"
 
-import { useEffect, useRef } from "react"
 import { Spinner } from "@/components/ui"
-import { UserAvatar } from "@/components/user/UserAvatar"
 import { useMessages } from "@/hooks/useMessages"
 import { usePreferredLanguage } from "@/hooks/usePreferredLanguage"
-import { cn } from "@/utilities/string"
-import { getLanguageCode } from "@/constants/languages"
 import { StartConversation } from "./StartConversation"
+import { ChatMessagesList } from "./ChatMessagesList"
 import type { Message } from "@/types/Message"
 import type { User } from "next-auth"
 
@@ -23,12 +20,7 @@ export const ChatMessages: React.FC<ChatMessagesProps> = ({
   user,
 }) => {
   const language = usePreferredLanguage()
-  const messagesEndRef = useRef<React.ElementRef<"div">>(null)
   const messages = useMessages(chatId, initialMessages)
-
-  useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
-  }, [messages])
 
   if (messages.status === "loading") {
     return <Spinner />
@@ -43,54 +35,10 @@ export const ChatMessages: React.FC<ChatMessagesProps> = ({
   }
 
   return (
-    <div className={cn("p-5", "flex-1")}>
-      {messages.data.map(message => {
-        const isSender = message.user.id === user.id
-        return (
-          <div
-            key={message.id}
-            className={cn("my-2", "flex items-end")}
-          >
-            <div
-              className={cn(
-                "mx-2",
-                "w-fit",
-                "p-4",
-                "line-clamp-1",
-                "rounded-lg",
-                "relative flex flex-col space-y-2",
-                isSender
-                  ? "ml-auto bg-cyan-500 dark:bg-indigo-600 text-white rounded-br-none"
-                  : "bg-gray-100 dark:text-gray-100 dark:bg-slate-700 rounded-bl-none"
-              )}
-            >
-              <p
-                className={cn(
-                  "text-xs italic font-extralight line-clamp-1",
-                  isSender ? "text-right" : "text-left"
-                )}
-              >
-                {message.user.name?.split(" ")[0]}
-              </p>
-              <div className="flex space-x-2">
-                {message.translated ? (
-                  <p>
-                    {message.translated?.[getLanguageCode(language)] ||
-                      message.input}
-                  </p>
-                ) : (
-                  <Spinner />
-                )}
-              </div>
-            </div>
-            <UserAvatar
-              name={message.user.name}
-              image={message.user.image}
-              className={cn({ "-order-1": !isSender })}
-            />
-          </div>
-        )
-      })}
-    </div>
+    <ChatMessagesList
+      language={language}
+      messages={messages.data}
+      user={user}
+    />
   )
 }
---
+++ src/components/chat/ChatMessagesList.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { useEffect, useRef } from "react"
+import { Spinner } from "@/components/ui"
+import { UserAvatar } from "@/components/user/UserAvatar"
+import { cn } from "@/utilities/string"
+import { getLanguageCode } from "@/constants/languages"
+import type { Language } from "@/types/Language"
+import type { Message } from "@/types/Message"
+import type { User } from "next-auth"
+
+export const ChatMessagesList: React.FC<{
+  language: Language
+  messages: Message[]
+  user: User
+}> = ({ language, messages, user }) => {
+  const messagesEndRef = useRef<React.ElementRef<"li">>(null)
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }, [messages])
+
+  return (
+    <ul className={cn("p-5", "flex-1")}>
+      {messages.map(message => {
+        const isSender = message.user.id === user.id
+        return (
+          <li
+            key={message.id}
+            className={cn("my-2", "flex items-end")}
+          >
+            <div
+              className={cn(
+                "mx-2",
+                "w-fit",
+                "p-4",
+                "line-clamp-1",
+                "rounded-lg",
+                "relative flex flex-col space-y-2",
+                isSender
+                  ? "ml-auto bg-cyan-500 dark:bg-indigo-600 text-white rounded-br-none"
+                  : "bg-gray-100 dark:text-gray-100 dark:bg-slate-700 rounded-bl-none"
+              )}
+            >
+              <p
+                className={cn(
+                  "text-xs italic font-extralight line-clamp-1",
+                  isSender ? "text-right" : "text-left"
+                )}
+              >
+                {message.user.name?.split(" ")[0]}
+              </p>
+              <div className="flex space-x-2">
+                {message.translated ? (
+                  <p>
+                    {message.translated?.[getLanguageCode(language)] ||
+                      message.input}
+                  </p>
+                ) : (
+                  <Spinner />
+                )}
+              </div>
+            </div>
+            <UserAvatar
+              name={message.user.name}
+              image={message.user.image}
+              className={cn({ "-order-1": !isSender })}
+            />
+          </li>
+        )
+      })}
+      <li
+        className="sr-only"
+        ref={messagesEndRef}
+      >
+        End of Messages
+      </li>
+    </ul>
+  )
+}

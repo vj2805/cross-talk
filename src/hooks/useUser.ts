105eafb5f4b347be24634784c2f76e9bd542aaa0
Vj2805 <vijk502@gmail.com>
2023-11-16 10:19:54 +0530

    useUser: doesnt throws

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,10 +1,5 @@
-import { UserError } from "@/errors/UserError"
 import { useUserStore } from "@/stores/user"
 
 export function useUser() {
-  const user = useUserStore(store => store.user)
-  if (!user) {
-    throw new UserError("User is NOT signed in!")
-  }
-  return user
+  return useUserStore(store => store.user)
 }

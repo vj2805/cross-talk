156ad2ea422f6b25b1b259c2bf1bd7081bae0326
Vj2805 <vijk502@gmail.com>
2023-11-16 14:10:29 +0530

    translations

---
+++ src/utilities/translations.ts
@@ -0,0 +1,142 @@
+import type { Language } from "@/types/Language"
+
+const TRANSLATIONS: Record<
+  Language,
+  Record<
+    | "Get the conversation started..."
+    | "Invite a friend"
+    | "Let's get you started by creating your first chat!"
+    | "No messages yet"
+    | "Send your first message in ANY language below to get started!"
+    | "The AI will auto-detect & translate it all for you..."
+    | "Welcome!",
+    string
+  >
+> = {
+  English: {
+    "Get the conversation started...": "Get the conversation started...",
+    "Invite a friend": "Invite a friend",
+    "Let's get you started by creating your first chat!":
+      "Let's get you started by creating your first chat!",
+    "No messages yet": "",
+    "Send your first message in ANY language below to get started!":
+      "Send your first message in ANY language below to get started!",
+    "The AI will auto-detect & translate it all for you...":
+      "The AI will auto-detect & translate it all for you...",
+    "Welcome!": "Welcome!",
+  },
+  French: {
+    "Get the conversation started...": "Lancez la conversation...",
+    "Invite a friend": "Invite un ami",
+    "Let's get you started by creating your first chat!":
+      "Commençons par créer votre premier converser!",
+    "No messages yet": "Pas encore de messages",
+    "Send your first message in ANY language below to get started!":
+      "Envoyez votre premier message dans N'IMPORTE QUELLE langue pour commencer",
+    "The AI will auto-detect & translate it all for you...":
+      "L' intelligence artificielle détectera et traduira automatiquement tout cela pour vous...",
+    "Welcome!": "Bienvenue!",
+  },
+  German: {
+    "Get the conversation started...": "Beginnen Sie das Gespräch...",
+    "Invite a friend": "Einen Freund einladen",
+    "Let's get you started by creating your first chat!":
+      "Beginnen wir mit der Erstellung Ihres ersten Chats!",
+    "No messages yet": "Noch keine Nachrichten",
+    "Send your first message in ANY language below to get started!":
+      "Senden Sie Ihre erste Nachricht in JEDER Sprache, um loszulegen",
+    "The AI will auto-detect & translate it all for you...":
+      "Die künstliche Intelligenz wird alles automatisch erkennen und für Sie übersetzen...",
+    "Welcome!": "Willkommen!",
+  },
+  Hindi: {
+    "Get the conversation started...": "बातचीत शुरू करें...",
+    "Invite a friend": "किसी मित्र को आमंत्रित करें",
+    "Let's get you started by creating your first chat!":
+      "आइए अपनी पहली चैट बनाकर शुरुआत करें!",
+    "No messages yet": "अभी तक कोई संदेश नहीं",
+    "Send your first message in ANY language below to get started!":
+      "आरंभ करने के लिए अपना पहला संदेश किसी भी भाषा में भेजें",
+    "The AI will auto-detect & translate it all for you...":
+      "कृत्रिम बुद्धिमत्ता स्वचालित रूप से यह सब पता लगाएगी और आपके लिए इसका अनुवाद करेगी...",
+    "Welcome!": "स्वागत!",
+  },
+  Japanese: {
+    "Get the conversation started...": "会話を始めましょう...",
+    "Invite a friend": "友達を招待する",
+    "Let's get you started by creating your first chat!":
+      "まずは最初のチャットを作成してみましょう",
+    "No messages yet": "まだメッセージはありません",
+    "Send your first message in ANY language below to get started!":
+      "まずは任意の言語で最初のメッセージを送信してください",
+    "The AI will auto-detect & translate it all for you...":
+      "人工知能がすべてを自動的に検出して翻訳します...",
+    "Welcome!": "いらっしゃいませ！",
+  },
+  Kannada: {
+    "Get the conversation started...": "ಸಂಭಾಷಣೆಯನ್ನು ಪ್ರಾರಂಭಿಸಿ...",
+    "Invite a friend": "ಸ್ನೇಹಿತನನ್ನು ಆಮಂತ್ರಿಸು",
+    "Let's get you started by creating your first chat!":
+      "ನಿಮ್ಮ ಮೊದಲ ಚಾಟ್ ರಚಿಸುವ ಮೂಲಕ ಪ್ರಾರಂಭಿಸೋಣ!",
+    "No messages yet": "ಇನ್ನೂ ಯಾವುದೇ ಸಂದೇಶಗಳಿಲ್ಲ",
+    "Send your first message in ANY language below to get started!":
+      "ಪ್ರಾರಂಭಿಸಲು ಯಾವುದೇ ಭಾಷೆಯಲ್ಲಿ ನಿಮ್ಮ ಮೊದಲ ಸಂದೇಶವನ್ನು ಕಳುಹಿಸಿ",
+    "The AI will auto-detect & translate it all for you...":
+      "ಕೃತಕ ಬುದ್ಧಿಮತ್ತೆ ಸ್ವಯಂಚಾಲಿತವಾಗಿ ಪತ್ತೆ ಮಾಡುತ್ತದೆ ಮತ್ತು ನಿಮಗಾಗಿ ಎಲ್ಲವನ್ನೂ ಅನುವಾದಿಸುತ್ತದೆ...",
+    "Welcome!": "ಸ್ವಾಗತ!",
+  },
+  Malayalam: {
+    "Get the conversation started...": "സംഭാഷണം ആരംഭിക്കുക...",
+    "Invite a friend": "ഒരു സുഹൃത്തിനെ ക്ഷണിക്കുക",
+    "Let's get you started by creating your first chat!":
+      "നിങ്ങളുടെ ആദ്യ ചാറ്റ് സൃഷ്ടിച്ചുകൊണ്ട് നമുക്ക് ആരംഭിക്കാം!",
+    "No messages yet": "ഇതുവരെ സന്ദേശങ്ങളൊന്നുമില്ല",
+    "Send your first message in ANY language below to get started!":
+      "ആരംഭിക്കുന്നതിന് ഏത് ഭാഷയിലും നിങ്ങളുടെ ആദ്യ സന്ദേശം അയയ്‌ക്കുക",
+    "The AI will auto-detect & translate it all for you...":
+      "ആർട്ടിഫിഷ്യൽ ഇന്റലിജൻസ് സ്വയമേവ കണ്ടെത്തുകയും നിങ്ങൾക്കായി വിവർത്തനം ചെയ്യുകയും ചെയ്യും...",
+    "Welcome!": "സ്വാഗതം!",
+  },
+  Spanish: {
+    "Get the conversation started...": "Inicie la conversación...",
+    "Invite a friend": "Invitar a un amigo",
+    "Let's get you started by creating your first chat!":
+      "¡Comencemos creando tu primer chat!",
+    "No messages yet": "No hay mensajes todavía",
+    "Send your first message in ANY language below to get started!":
+      "Envía tu primer mensaje en CUALQUIER idioma para comenzar",
+    "The AI will auto-detect & translate it all for you...":
+      "La inteligencia artificial lo detectará y traducirá todo automáticamente...",
+    "Welcome!": "¡Bienvenido!",
+  },
+  Tamil: {
+    "Get the conversation started...": "உரையாடலைத் தொடங்கவும்...",
+    "Invite a friend": "நண்பரை அழை",
+    "Let's get you started by creating your first chat!":
+      "உங்கள் முதல் அரட்டையை உருவாக்குவதன் மூலம் தொடங்குவோம்!",
+    "No messages yet": "இதுவரை எந்தச் செய்தியும் இல்லை",
+    "Send your first message in ANY language below to get started!":
+      "தொடங்குவதற்கு உங்கள் முதல் செய்தியை ஏதோர் மொழியிலும் அனுப்பவும்",
+    "The AI will auto-detect & translate it all for you...":
+      "செயற்கை நுண்ணறிவு தானாகவே உங்களுக்காக அனைத்தையும் மொழிபெயர்த்துவிடும்...",
+    "Welcome!": "நல்வரவு!",
+  },
+  Telugu: {
+    "Get the conversation started...": "సంభాషణను ప్రారంభించండి...",
+    "Invite a friend": "స్నేహితుడిని ఆహ్వానించు",
+    "Let's get you started by creating your first chat!":
+      "మీ మొదటి చాట్‌ని సృష్టించడం ద్వారా ప్రారంభించండి!",
+    "No messages yet": "ఇంకా సందేశాలు లేవు",
+    "Send your first message in ANY language below to get started!":
+      "ప్రారంభించడానికి మీ మొదటి సందేశాన్ని ఏదైనా భాషలో పంపండి",
+    "The AI will auto-detect & translate it all for you...":
+      "కృత్రిమ మేధస్సు స్వయంచాలకంగా గుర్తించి మీ కోసం అన్నింటినీ అనువదిస్తుంది...",
+    "Welcome!": "స్వాగతం!",
+  },
+}
+
+export function getTranslation<
+  Key extends keyof (typeof TRANSLATIONS)[Language],
+>(key: Key, language: Language) {
+  return TRANSLATIONS[language][key]
+}

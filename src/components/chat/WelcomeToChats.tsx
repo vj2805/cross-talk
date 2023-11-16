3ddeaf81ad56ce475084bb75424752f6be059b16
Vj2805 <vijk502@gmail.com>
2023-11-16 12:14:15 +0530

    WelcomeToChats: renders based on language

--- src/components/chat/WelcomeToChats.tsx
+++ src/components/chat/WelcomeToChats.tsx
@@ -1,19 +1,26 @@
 import { cn } from "@/utilities/string"
+import { getWelcome } from "@/constants/welcomes"
 import { MessageSquareIcon } from "../ui/icons"
 import { CreateChatButton } from "./CreateChatButton"
+import type { Language } from "@/types/Language"
 
-export const WelcomeToChats: React.FC = () => (
-  <div
-    className={cn(
-      "pt-40",
-      "flex flex-col justify-center items-center space-y-2"
-    )}
-  >
-    <MessageSquareIcon className="h-10 w-10" />
-    <h1 className="text-5xl font-extralight">Welcome!</h1>
-    <h2 className="pb-10">
-      Let&apos;s get you started by creating your first chat!
-    </h2>
-    <CreateChatButton large />
-  </div>
-)
+interface WelcomeToChatsProps {
+  language: Language
+}
+
+export const WelcomeToChats: React.FC<WelcomeToChatsProps> = ({ language }) => {
+  const welcome = getWelcome(language)
+  return (
+    <div
+      className={cn(
+        "pt-40",
+        "flex flex-col justify-center items-center space-y-2"
+      )}
+    >
+      <MessageSquareIcon className="h-10 w-10" />
+      <h1 className="text-5xl font-extralight">{welcome.greeting}</h1>
+      <h2 className="pb-10">{welcome.message}</h2>
+      <CreateChatButton large />
+    </div>
+  )
+}

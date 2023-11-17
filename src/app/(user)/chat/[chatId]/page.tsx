9a71b58610bf4085292e00b81483aec914de8736
Vj2805 <vijk502@gmail.com>
2023-11-17 22:00:46 +0530

    ChatPage: no longer provides initialMessages

--- src/app/(user)/chat/[chatId]/page.tsx
+++ src/app/(user)/chat/[chatId]/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation"
 import { ChatInput, ChatMessages } from "@/components"
 import { ChatControls } from "@/components/chat/ChatControls"
 import { getServerUser } from "@/services/auth"
-import { getMessages } from "@/services/message"
 
 interface ChatPageProps {
   params: {
@@ -17,15 +16,13 @@ export default async function ChatPage({ params: { chatId } }: ChatPageProps) {
     return redirect("/")
   }
 
-  let initialMessages = await getMessages({ chatId })
-
   return (
     <>
       <ChatControls chatId={chatId} />
       <ChatMessages
         chatId={chatId}
         user={user}
-        initialMessages={initialMessages}
+        initialMessages={[]}
       />
       <ChatInput chatId={chatId} />
     </>

c04cff9df1f29ae181dc292e7b3dbac5b6194d8e
Vj2805 <vijk502@gmail.com>
2023-11-17 21:29:04 +0530

    AdminControls: added DeleteChatButton

--- src/components/chat/AdminControls.tsx
+++ src/components/chat/AdminControls.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/utilities/string"
 import { InviteUser } from "./InviteUser"
 import { ShareLink } from "./ShareLink"
+import { DeleteChatButton } from "./DeleteChatButton"
 import type { Chat } from "@/types/Chat"
 
 interface AdminControlsProps {
@@ -11,5 +12,9 @@ export const AdminControls: React.FC<AdminControlsProps> = ({ chat }) => (
   <div className={cn("m-5 mb-0", "flex justify-end space-x-2")}>
     <InviteUser chat={chat} />
     <ShareLink chatId={chat.id} />
+    <DeleteChatButton
+      chatId={chat.id}
+      adminId={chat.adminId}
+    />
   </div>
 )

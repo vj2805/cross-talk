c8e156754e978b4cbaaaf976009ce098a0c6fe84
Vj2805 <vijk502@gmail.com>
2023-11-17 16:29:44 +0530

    Service: Subscribe

--- src/types/Service.d.ts
+++ src/types/Service.d.ts
@@ -10,10 +10,15 @@ export type Mutate<Parameters extends object, Returns = void> = (
   parameters: Parameters
 ) => Promise<Returns>
 
-export type Subscribe<Parameters extends object, Observable> = (
-  parameters: Parameters,
-  onChange: Consumer<Observable>,
-  onError?: Consumer<Error>
-) => Unsubscribe
+export type Subscribe<
+  Parameters extends object | null,
+  Observable,
+> = Parameters extends null
+  ? (onChange: Consumer<Observable>, onError?: Consumer<Error>) => Unsubscribe
+  : (
+      parameters: Parameters,
+      onChange: Consumer<Observable>,
+      onError?: Consumer<Error>
+    ) => Unsubscribe
 
 export type Unsubscribe = () => void

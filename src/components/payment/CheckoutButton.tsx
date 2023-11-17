a60c3fe3b5190ba9ff0940d7e23e09060d054a80
Vj2805 <vijk502@gmail.com>
2023-11-18 00:43:02 +0530

    CheckoutButton

--- src/components/payment/CheckoutButton.tsx
+++ src/components/payment/CheckoutButton.tsx
@@ -22,17 +22,25 @@ export const CheckoutButton: React.FC<CheckoutButtonProps> = ({ priceId }) => {
 
   return (
     <div className="flex flex-col space-y-2">
+      {subscription?.status === "active" && (
+        <>
+          <hr className="mt-5" />
+          <p className="pt-5 text-center text-xs text-indigo-600">
+            You are subscribed to PRO
+          </p>
+        </>
+      )}
       <div
         className={cn(
-          "mt-8",
+          "mt-8 flex",
           "block",
           "px-3.5 py-2",
-          "bg-cyan-500 hover:bg-cyan-400 disabled:bg-cyan-500/50",
+          "bg-indigo-500 hover:bg-indigo-600 disabled:bg-indigo-500/50",
           "text-center text-sm font-semibold leading-6 text-white disabled:text-white",
           "rounded-md",
           "shadow-sm",
           "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-500",
-          "cursor-pointer  disabled:cursor-default",
+          "cursor-pointer disabled:cursor-default",
           "disabled:opacity-80"
         )}
       >
@@ -41,9 +49,12 @@ export const CheckoutButton: React.FC<CheckoutButtonProps> = ({ priceId }) => {
         ) : subscription?.status === "active" ? (
           <ManageSubscriptionButton />
         ) : (
-          <button onClick={() => createCheckout(user.id, priceId)}>
-            Subscribe
-          </button>
+          <form
+            className="flex"
+            onSubmit={() => createCheckout(user.id, priceId)}
+          >
+            <button className="flex-1">Subscribe</button>
+          </form>
         )}
       </div>
     </div>

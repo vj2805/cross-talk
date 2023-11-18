829ee3704e6b3bb4c5b3a05d4578bab8be88f9f7
Vj2805 <vijk502@gmail.com>
2023-11-18 09:54:20 +0530

    useCreateChat: restricted based on subscription

--- src/hooks/useCreateChat.ts
+++ src/hooks/useCreateChat.ts
@@ -1,26 +1,37 @@
 import { showToast } from "@/components/ui"
-import { createChat } from "@/services/chat"
+import { createChat, getParticipatingChatCount } from "@/services/chat"
 import { sleep } from "@/utilities/sleep"
+import { FreePlanLimitExceededError } from "@/errors/FreePlanLimitExceededError"
 import { useRouter } from "./useBuiltins"
 import { useProcess } from "./useProcess"
+import { useIsPro } from "./useIsPro"
 
 export function useCreateChat() {
+  const isPro = useIsPro()
   const router = useRouter()
   return useProcess(async (_, adminId: string) => {
     showToast({
       description: "Hold tight while we create your new chat...",
+      duration: 2000,
       title: "Creating new chat...",
       variant: "default",
     })
     try {
-      await sleep(1750)
+      if (!isPro) {
+        const participatingChatCount = await getParticipatingChatCount({
+          userId: adminId,
+        })
+        if (participatingChatCount >= 3) {
+          throw new FreePlanLimitExceededError("3 chats per user")
+        }
+      }
       const chatId = await createChat({ adminId })
       showToast({
         description: "Redirecting to the new chat, Please wait...",
+        duration: 2000,
         title: "Created new chat!",
         variant: "success",
       })
-      await sleep(1750)
       router.push(`/chat/${chatId}`)
     } catch (error) {
       showToast({ error: error as Error })

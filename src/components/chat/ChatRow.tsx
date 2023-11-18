56e6d946f6e259c7ac2b667ade79dbebeff2a3ea
Vj2805 <vijk502@gmail.com>
2023-11-18 09:35:49 +0530

    ChatRow: inlined

--- src/components/chat/ChatRow.tsx
+++ src/components/chat/ChatRow.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import { ChatRowSkeleton } from "@/components/chat/ChatRowSkeleton"
+import { showToast } from "@/components/ui"
+import { UserAvatar } from "@/components/user/UserAvatar"
 import { useRouter } from "@/hooks/useBuiltins"
 import { useLastMessage } from "@/hooks/useLastMessage"
+import { usePreferredLanguage } from "@/hooks/usePreferredLanguage"
 import { useRequiredUser } from "@/hooks/useRequiredUser"
-import { cn } from "@/utilities/string"
-import { showToast } from "../ui"
-import { ChatRowSkeleton } from "./ChatRowSkeleton"
-import { ChatRowThatHasNoMessages } from "./ChatRowThatHasNoMessages"
-import { ChatRowWithLastMessage } from "./ChatRowWithLastMessage"
+import { useTranslate } from "@/hooks/useTranslate"
+import { getLanguageCode } from "@/utilities/languages"
+import { cn, prettifyId } from "@/utilities/string"
+import { getTimestampString } from "@/utilities/timestamps"
 import type { Chat } from "@/types/Chat"
 
 interface ChatRowProps {
@@ -18,6 +21,8 @@ export const ChatRow: React.FC<ChatRowProps> = ({ chatId }) => {
   const [lastMessage, status, error] = useLastMessage(chatId)
   const [user] = useRequiredUser()
   const router = useRouter()
+  const language = usePreferredLanguage()
+  const translate = useTranslate()
 
   if (!user) {
     return null
@@ -41,18 +46,33 @@ export const ChatRow: React.FC<ChatRowProps> = ({ chatId }) => {
         "cursor-pointer"
       )}
     >
-      {!lastMessage ? (
-        <ChatRowThatHasNoMessages
-          chatId={chatId}
-          user={user}
-        />
-      ) : (
-        <ChatRowWithLastMessage
-          chatId={chatId}
-          lastMessage={lastMessage}
-          user={user}
-        />
-      )}
+      <UserAvatar
+        name={lastMessage?.user.name ?? user.name}
+        image={lastMessage?.user.image ?? user.image}
+      />
+      <div className="flex-1">
+        <p className="font-bold">
+          {lastMessage
+            ? (lastMessage.user.name ?? user.name)?.split(" ")[0]
+            : translate("New Chat")}
+        </p>
+        <p className="text-gray-400 line-clamp-1">
+          {lastMessage
+            ? lastMessage.translated?.[getLanguageCode(language)] ??
+              lastMessage.input
+            : translate("Get the conversation started...")}
+        </p>
+      </div>
+      <div className="text-xs text-gray-400 text-right">
+        <p className="mb-auto">
+          {lastMessage
+            ? getTimestampString(lastMessage.timestamp)
+            : translate("No messages yet")}
+        </p>
+        <p className="font-thin">
+          {translate("Chat")} #{prettifyId(chatId)}...
+        </p>
+      </div>
     </div>
   )
 }

31d5d40c27795c06cee25236265cc82e405e42ce
Vj2805 <vijk502@gmail.com>
2023-11-18 13:31:38 +0530

    inmemory/participant:

---
+++ src/services/internal/inmemory/participant.ts
@@ -0,0 +1,33 @@
+import { ChatError } from "@/errors/ChatError"
+import { UserError } from "@/errors/UserError"
+import { get, set } from "./store"
+import type { ParticipantService } from "@/types/ParticipantService"
+
+const inmemoryParticipantService: ParticipantService = {
+  async addParticipantToChat({ chatId, participantId }) {
+    const participant = get("users").some(user => user.id === participantId)
+    if (!participant) {
+      throw new UserError("User does not exist!")
+    }
+    const chat = get("chats").find(chat => chat.id === chatId)
+    if (!chat) {
+      throw new ChatError("Chat does not exist!")
+    }
+    set("chats", chats =>
+      chats.map(chat =>
+        chat.id !== chatId
+          ? chat
+          : {
+              ...chat,
+              participantsIds: chat.participantsIds.toSpliced(
+                -1,
+                0,
+                participantId
+              ),
+            }
+      )
+    )
+  },
+}
+
+export default inmemoryParticipantService

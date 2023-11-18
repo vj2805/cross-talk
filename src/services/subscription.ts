a7e760ea56a74a586002564304be59a8d49a3441
Vj2805 <vijk502@gmail.com>
2023-11-18 14:48:29 +0530

    services: uses internal/firebase

--- src/services/subscription.ts
+++ src/services/subscription.ts
@@ -1,4 +1,4 @@
-import { default as subscriptionService } from "./internal/inmemory/subscription"
+import { default as subscriptionService } from "./internal/firebase/subscription"
 import type { SubscriptionService } from "@/types/SubscriptionService"
 
 export const { syncSubscription }: SubscriptionService = subscriptionService

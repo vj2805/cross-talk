413beaeba14e0db064aa45c0707e04e4398589b3
Vj2805 <vijk502@gmail.com>
2023-11-23 19:26:45 +0530

    actions/deleteChat

--- src/actions/deleteChat.ts
+++ src/actions/deleteChat.ts
@@ -4,11 +4,18 @@ import { maximumRetryAttempts } from "@/configs/defaults"
 import { adminRepo } from "@/configs/firebase/admin"
 
 export async function deleteChat(chatId: string) {
-  const bulkWriter = adminRepo.bulkWriter()
+  return await new Promise<void>(async (resolve, reject) => {
+    const bulkWriter = adminRepo.bulkWriter()
+    bulkWriter.onWriteError(error => {
+      if (error.failedAttempts < maximumRetryAttempts) {
+        return true
+      }
+      reject(new Error(`[deleteChat] ${error.name} ${error.message}`))
+      return false
+    })
 
-  bulkWriter.onWriteError(error => error.failedAttempts < maximumRetryAttempts)
-
-  const chatRef = adminRepo.collection("chats").doc(chatId)
-
-  await adminRepo.recursiveDelete(chatRef, bulkWriter)
+    const chatRef = adminRepo.collection("chats").doc(chatId)
+    await adminRepo.recursiveDelete(chatRef, bulkWriter)
+    resolve()
+  })
 }

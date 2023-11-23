8b2af5580a076295690827c3e88224121a2c4929
Vj2805 <vijk502@gmail.com>
2023-11-23 10:49:11 +0530

    services/language: merged

--- src/services/language.ts
+++ src/services/language.ts
@@ -1,13 +1,33 @@
-import type { LanguageService } from "@/types/LanguageService"
-import { getAvailableLanguages as getAllAvailableLanguages } from "@/utilities/languages"
+import type {
+  AvailableLanguages,
+  Language,
+  LanguageCode,
+} from "@/types/Language"
 
-const NO_OF_LANGUAGES_IN_FREE = 2
+const FREE_LANGUAGES_COUNT = 2
 
-export const { getAvailableLanguages }: LanguageService = {
-  async getAvailableLanguages() {
-    return {
-      free: getAllAvailableLanguages().slice(0, NO_OF_LANGUAGES_IN_FREE),
-      pro: getAllAvailableLanguages().slice(NO_OF_LANGUAGES_IN_FREE),
-    }
-  },
+const CODES: Record<Language, LanguageCode> = {
+  English: "en",
+  French: "fr",
+  German: "de",
+  Hindi: "hi",
+  Japanese: "ja",
+  Kannada: "kn",
+  Malayalam: "ml",
+  Spanish: "es",
+  Tamil: "ta",
+  Telugu: "te",
+}
+
+const LANGUAGES: Language[] = Object.keys(CODES) as Language[]
+
+export function getLanguageCode(language: Language): LanguageCode {
+  return CODES[language]
+}
+
+export async function getAvailableLanguages(): Promise<AvailableLanguages> {
+  return [
+    LANGUAGES.slice(0, FREE_LANGUAGES_COUNT),
+    LANGUAGES.slice(FREE_LANGUAGES_COUNT),
+  ]
 }

a6d80d0c60ea308a145007128c2dd1ac1f6fdeff
Vj2805 <vijk502@gmail.com>
2023-11-23 10:52:05 +0530

    types: deleted AuthService, ChatService, LanguageService

--- src/types/LanguageService.ts
+++
@@ -1,6 +0,0 @@
-import type { AvailableLanguages } from "./Language"
-import type { Obtain } from "./Service"
-
-export interface LanguageService {
-  getAvailableLanguages: Obtain<AvailableLanguages>
-}

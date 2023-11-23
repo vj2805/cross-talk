049219646279e6f657e4eea73d34e000c695ae35
Vj2805 <vijk502@gmail.com>
2023-11-23 18:27:04 +0530

    usePreferredLanguage

--- src/hooks/usePreferredLanguage.ts
+++ src/hooks/usePreferredLanguage.ts
@@ -1,13 +1,13 @@
 import { shallow } from "zustand/shallow"
 import { useStore } from "@/stores/store"
 
-export function usePreferredLanguageCode() {
+export function usePreferredLanguage() {
   return useStore(store => {
     switch (store.status) {
       case "error":
         return [undefined, store.status, store.error] as const
       case "ready":
-        return [store.language.preferred.code, store.status, undefined] as const
+        return [store.language.preferred, store.status, undefined] as const
       case "loading":
         return [undefined, store.status, undefined] as const
     }

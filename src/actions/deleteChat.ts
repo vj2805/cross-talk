8e2b54405bea65f6e8e194b8073b836ce7c0f799
Vj2805 <vijk502@gmail.com>
2023-11-23 08:38:24 +0530

    actions: simplified

--- src/actions/deleteChat.ts
+++ src/actions/deleteChat.ts
@@ -1,1 +1,15 @@
-export { deleteChat } from "./internal/firebase/deleteChat"
+"use server"
+
+import { adminRepo } from "@/backend/firebase/admin"
+
+const MAX_RETRY_ATTEMPTS = 5
+
+export async function deleteChat(chatId: string) {
+  const bulkWriter = adminRepo.bulkWriter()
+
+  bulkWriter.onWriteError(error => error.failedAttempts < MAX_RETRY_ATTEMPTS)
+
+  const chatRef = adminRepo.collection("chats").doc(chatId)
+
+  await adminRepo.recursiveDelete(chatRef, bulkWriter)
+}

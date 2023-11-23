ac749ebef34bab1fdebdf04284ccf04bb9ee29fd
Vj2805 <vijk502@gmail.com>
2023-11-23 09:34:01 +0530

    services/inmemory/chat: combined into services/chat

--- src/services/chat.ts
+++ src/services/chat.ts
@@ -1,5 +1,50 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  documentId,
+  getCountFromServer,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore"
+import type { FirestoreDataConverter } from "firebase/firestore"
+import { clientRepo } from "@/backend/firebase/client"
+import type { Chat } from "@/types/Chat"
 import type { ChatService } from "@/types/ChatService"
-import { default as chatService } from "./internal/firebase/chat"
+
+const chatConverter: FirestoreDataConverter<Chat> = {
+  fromFirestore(snapshot, options) {
+    const data = snapshot.data(options)
+    return {
+      adminId: data.adminId,
+      id: snapshot.id,
+      participantsIds: data.participantsIds,
+    }
+  },
+  toFirestore(chat) {
+    return {
+      adminId: chat.adminId,
+      participantsIds: chat.participantsIds,
+    }
+  },
+}
+
+export function chatsRef() {
+  return collection(clientRepo, "chats").withConverter(chatConverter)
+}
+
+export function chatRef(chatId: string) {
+  return doc(chatsRef(), chatId)
+}
+
+export function participatingChatsRef(participantId: string) {
+  return query(
+    chatsRef(),
+    where("participantsIds", "array-contains", participantId)
+  )
+}
 
 export const {
   createChat,
@@ -7,4 +52,42 @@ export const {
   getParticipatingChats,
   subscribeToChat,
   subscribeToParticipatingChats,
-}: ChatService = chatService
+}: ChatService = {
+  async createChat({ adminId }) {
+    const chatRef = await addDoc(chatsRef(), {
+      adminId,
+      id: documentId(),
+      participantsIds: [adminId],
+    })
+    return chatRef.id
+  },
+  async getParticipatingChatCount({ userId }) {
+    const snapshot = await getCountFromServer(participatingChatsRef(userId))
+    const aggregate = snapshot.data()
+    return aggregate.count
+  },
+  async getParticipatingChats({ userId }) {
+    const snapshot = await getDocs(participatingChatsRef(userId))
+    return snapshot.docs.map(doc => doc.data())
+  },
+  subscribeToChat({ chatId }, onChange, onError) {
+    return onSnapshot(
+      chatRef(chatId),
+      snapshot => {
+        const chat = snapshot.data()
+        if (!chat) {
+          return
+        }
+        return onChange(chat)
+      },
+      onError
+    )
+  },
+  subscribeToParticipatingChats({ userId }, onChange, onError) {
+    return onSnapshot(
+      participatingChatsRef(userId),
+      snapshot => onChange(snapshot.docs.map(doc => doc.data())),
+      onError
+    )
+  },
+}

460a01e2f289c2f6b589bb846748d96908234a59
Vj2805 <vijk502@gmail.com>
2023-11-23 12:08:14 +0530

    firebase/admin:

--- src/configs/firebase/admin.ts
+++ src/configs/firebase/admin.ts
@@ -1,17 +1,18 @@
-import * as admin from "firebase-admin"
+import { cert, getApp, getApps, initializeApp } from "firebase-admin/app"
+import type { AppOptions } from "firebase-admin/app"
+import { getAuth } from "firebase-admin/auth"
+import { getFirestore } from "firebase-admin/firestore"
 import { env } from "@/configs/env"
 
-const adminAppOptions: admin.AppOptions = {
-  credential: admin.credential.cert({
+const adminAppOptions: AppOptions = {
+  credential: cert({
     clientEmail: env["FIREBASE_CLIENT_EMAIL"],
     privateKey: env["FIREBASE_PRIVATE_KEY"],
     projectId: env["FIREBASE_PROJECT_ID"],
   }),
 }
 
-const adminApp = admin.apps.length
-  ? admin.app()
-  : admin.initializeApp(adminAppOptions)
+const adminApp = getApps().length ? getApp() : initializeApp(adminAppOptions)
 
-export const adminAuth = admin.auth(adminApp)
-export const adminRepo = admin.firestore(adminApp)
+export const adminAuth = getAuth(adminApp)
+export const adminRepo = getFirestore(adminApp)

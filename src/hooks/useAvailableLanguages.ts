275091977026f668a02abe5e4a91cf430a47f725
Vj2805 <vijk502@gmail.com>
2023-11-23 13:29:17 +0530

    useAvailableLanguages: uses useStore

--- src/hooks/useAvailableLanguages.ts
+++ src/hooks/useAvailableLanguages.ts
@@ -1,35 +1,17 @@
 import { shallow } from "zustand/shallow"
-import { useLanguageStore } from "@/stores/language"
-import { useIsPro } from "./useIsPro"
+import { useStore } from "@/stores/store"
 
 export function useAvailableLanguages() {
-  const [isPro, subscriptionStatus, subscriptionError] = useIsPro()
-  return useLanguageStore(store => {
-    if (subscriptionStatus === "loading") {
-      return [undefined, undefined, "loading", undefined] as const
+  return useStore(store => {
+    switch (store.status) {
+      case "error":
+        return [undefined, undefined, store.status, store.error] as const
+      case "idle": {
+        const { supported, unsupported } = store.language
+        return [supported, unsupported, "idle", undefined] as const
+      }
+      case "loading":
+        return [undefined, undefined, store.status, undefined] as const
     }
-    if (subscriptionStatus === "error") {
-      return [undefined, undefined, "error", subscriptionError] as const
-    }
-
-    const [
-      availableLanguages,
-      availableLanguagesStatus,
-      availableLanguagesError,
-    ] = store.availableLanguages
-
-    if (availableLanguagesStatus === "loading") {
-      return [undefined, undefined, "loading", undefined] as const
-    }
-    if (availableLanguagesStatus === "error") {
-      return [undefined, undefined, "error", availableLanguagesError] as const
-    }
-
-    return [
-      availableLanguages[0].concat(isPro ? availableLanguages[1] : []),
-      isPro ? [] : availableLanguages[1],
-      "idle",
-      undefined,
-    ] as const
   }, shallow)
 }

bd03718203a710eb662f9d882831a634d4e34ef6
Vj2805 <vijk502@gmail.com>
2023-11-23 16:44:12 +0530

    SyncProvider

--- src/components/providers/SyncProvider.tsx
+++ src/components/providers/SyncProvider.tsx
@@ -1,12 +1,33 @@
 "use client"
 
-import { useSyncSubscription } from "@/hooks/useSyncSubscription"
-import { useSyncUser } from "@/hooks/useSyncUser"
+import { useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { syncSubscription } from "@/services/subscription"
+import { syncUser } from "@/services/user"
+import { setIsPro, setLoading } from "@/stores/store"
 
 export const SyncProvider: React.FC<
   React.PropsWithRequiredChildren
 > = props => {
-  useSyncUser()
-  useSyncSubscription()
+  const { data: session, status } = useSession()
+
+  // Syncing user
+  useEffect(() => {
+    syncUser({ session })
+  }, [session])
+
+  // Syncing subscription
+  useEffect(() => {
+    if (status === "loading") {
+      return setLoading()
+    }
+    if (!session?.user) {
+      return setIsPro(false)
+    }
+    return syncSubscription({ userId: session.user.id }, subscription =>
+      setIsPro(subscription?.status === "active")
+    )
+  }, [session, status])
+
   return props.children
 }

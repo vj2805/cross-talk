e335240ce4f9760c9a71f960bab88746d157e997
Vj2805 <vijk502@gmail.com>
2023-11-24 14:00:58 +0530

    global: created Optional

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,7 @@
 import type { VariantProps } from "class-variance-authority"
 
 declare global {
+  type Optional<T> = T | undefined
   namespace React {
     type PropsWithRequiredChildren = Required<React.PropsWithChildren>
     type PropsWithAsChild<T extends object> = T & { asChild?: boolean }

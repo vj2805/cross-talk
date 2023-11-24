f54107477feac877d8c9eb1f21e43635a5281a7e
Vj2805 <vijk502@gmail.com>
2023-11-24 15:48:46 +0530

    styles

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -2,7 +2,7 @@ export default function UserLayout({
   children,
 }: React.PropsWithRequiredChildren) {
   return (
-    <div className="mx-auto w-full max-w-6xl flex-1 flex flex-col">
+    <div className="mx-auto w-full max-w-6xl py-2 flex-1 flex flex-col">
       {children}
     </div>
   )
